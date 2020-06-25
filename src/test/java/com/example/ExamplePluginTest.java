07f0c93c48870ee348b5dc060a5d805bf03cf70d
Zoinkwiz <zoinkwiz@hotmail.co.uk>
2020-06-25 09:37:22 +0100

    Initial commit

---
+++ src/test/java/com/example/ExamplePluginTest.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import net.runelite.client.RuneLite;
+import net.runelite.client.externalplugins.ExternalPluginManager;
+
+public class ExamplePluginTest
+{
+	public static void main(String[] args) throws Exception
+	{
+		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		RuneLite.main(args);
+	}
+}

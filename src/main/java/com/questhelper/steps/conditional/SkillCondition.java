1632f1ca9f6abf6ffcdc6c5db66d9ec723707b59
Zoinkwiz <zoinkwiz@hotmail.co.uk>
2020-07-01 11:50:17 +0100

    Added skill conditional

---
+++ src/main/java/com/questhelper/steps/conditional/SkillCondition.java
@@ -0,0 +1,47 @@
+package com.questhelper.steps.conditional;
+
+import net.runelite.api.Client;
+import net.runelite.api.Skill;
+
+public class SkillCondition extends ConditionForStep
+{
+
+	private final Skill skill;
+	private final int value;
+	private final Operation operation;
+
+
+	public SkillCondition(Skill skill, int value)
+	{
+		this.skill = skill;
+		this.value = value;
+		this.operation = Operation.EQUAL;
+	}
+
+	public SkillCondition(Skill skill, int value, Operation operation)
+	{
+		this.skill = skill;
+		this.value = value;
+		this.operation = operation;
+	}
+
+	@Override
+	public boolean checkCondition(Client client)
+	{
+		if (operation == Operation.EQUAL)
+		{
+			return client.getRealSkillLevel(skill) == value;
+		}
+		else if (operation == Operation.LESS_EQUAL)
+		{
+			return client.getRealSkillLevel(skill) <= value;
+		}
+
+		else if (operation == Operation.GREATER_EQUAL)
+		{
+			return client.getRealSkillLevel(skill) >= value;
+		}
+
+		return false;
+	}
+}

da986409225226a94063c441bbdf44eb6ebf0bcf
Zoinkwiz <zoinkwiz@hotmail.co.uk>
2020-09-13 22:40:01 +0100

    Add EmoteStep

---
+++ src/main/java/com/questhelper/steps/EmoteStep.java
@@ -0,0 +1,115 @@
+/*
+ * Copyright (c) 2020, Zoinkwiz <https://github.com/Zoinkwiz>
+ * Copyright (c) 2018, Lotto <https://github.com/devLotto>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.questhelper.steps;
+
+import com.questhelper.QuestHelperPlugin;
+import com.questhelper.questhelpers.QuestHelper;
+import com.questhelper.requirements.Requirement;
+import java.awt.Color;
+import java.awt.Graphics2D;
+import net.runelite.api.ScriptID;
+import net.runelite.api.coords.WorldPoint;
+import net.runelite.api.widgets.Widget;
+import net.runelite.api.widgets.WidgetInfo;
+import net.runelite.client.plugins.cluescrolls.clues.emote.Emote;
+
+public class EmoteStep extends DetailedQuestStep
+{
+	private boolean hasScrolled;
+	private final Emote emote;
+
+	public EmoteStep(QuestHelper questHelper, Emote emote, String text, Requirement... requirements)
+	{
+		super(questHelper, text, requirements);
+		this.emote = emote;
+	}
+
+	public EmoteStep(QuestHelper questHelper, Emote emote, WorldPoint worldPoint, String text, Requirement... requirements)
+	{
+		super(questHelper, worldPoint, text, requirements);
+		this.emote = emote;
+	}
+
+	@Override
+	public void makeWidgetOverlayHint(Graphics2D graphics, QuestHelperPlugin plugin)
+	{
+		super.makeWidgetOverlayHint(graphics, plugin);
+
+		Widget emoteContainer = client.getWidget(WidgetInfo.EMOTE_CONTAINER);
+
+		if (emoteContainer == null || emoteContainer.isHidden())
+		{
+			return;
+		}
+
+		Widget emoteWindow = client.getWidget(WidgetInfo.EMOTE_WINDOW);
+
+		if (emoteWindow == null)
+		{
+			return;
+		}
+
+		Widget finalEmoteWidget = null;
+
+		for (Widget emoteWidget : emoteContainer.getDynamicChildren())
+		{
+			if (emoteWidget.getSpriteId() == emote.getSpriteId())
+			{
+				finalEmoteWidget = emoteWidget;
+
+				graphics.setColor(new Color(0, 255, 255, 65));
+				graphics.fill(emoteWidget.getBounds());
+				graphics.setColor(Color.CYAN);
+				graphics.draw(emoteWidget.getBounds());
+			}
+		}
+		if (!hasScrolled)
+		{
+			hasScrolled = true;
+			scrollToWidget(WidgetInfo.EMOTE_CONTAINER, WidgetInfo.EMOTE_SCROLLBAR, finalEmoteWidget);
+		}
+	}
+
+	void scrollToWidget(WidgetInfo list, WidgetInfo scrollbar, Widget widget)
+	{
+		final Widget parent = client.getWidget(list);
+
+		if (widget == null)
+		{
+			return;
+		}
+
+		final int newScroll = Math.max(0, Math.min(parent.getScrollHeight(),
+			(widget.getRelativeY() + widget.getHeight() / 2) - parent.getHeight() / 2));
+
+		client.runScript(
+			ScriptID.UPDATE_SCROLLBAR,
+			scrollbar.getId(),
+			list.getId(),
+			newScroll
+		);
+	}
+}

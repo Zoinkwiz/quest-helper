d8eb1869d369ddf14489591bca9c6667c627681e
Zoinkwiz <zoinkwiz@hotmail.co.uk>
2020-10-17 15:36:36 +0100

    Added ability to restrict region of a widget text condition

--- src/main/java/com/questhelper/steps/conditional/WidgetTextCondition.java
+++ src/main/java/com/questhelper/steps/conditional/WidgetTextCondition.java
@@ -41,6 +41,10 @@ public class WidgetTextCondition extends ConditionForStep
 	private int childChildId = -1;
 	private boolean checkChildren;
 
+	// Used to restrict the considered set of children
+	private int min = -1;
+	private int max = -1;
+
 	public WidgetTextCondition(WidgetInfo widgetInfo, String... text)
 	{
 
@@ -85,6 +89,12 @@ public class WidgetTextCondition extends ConditionForStep
 		this.text = new ArrayList<>(Arrays.asList(text));
 	}
 
+	public void addRange(int min, int max)
+	{
+		this.min = min;
+		this.max = max;
+	}
+
 	@Override
 	public boolean checkCondition(Client client)
 	{
@@ -93,28 +103,6 @@ public class WidgetTextCondition extends ConditionForStep
 			return true;
 		}
 		return checkWidget(client);
-	}
-
-	private boolean getChildren(Widget parentWidget, String textOption)
-	{
-		for (Widget currentWidget : parentWidget.getStaticChildren())
-		{
-			if (currentWidget.getNestedChildren() != null)
-			{
-				if (currentWidget.getText().contains(textOption))
-				{
-					return true;
-				}
-			}
-			else
-			{
-				if (getChildren(currentWidget, textOption))
-				{
-					return true;
-				}
-			}
-		}
-		return false;
 	}
 
 	public boolean checkWidget(Client client)
@@ -142,6 +130,48 @@ public class WidgetTextCondition extends ConditionForStep
 					{
 						return true;
 					}
+				}
+			}
+		}
+		return false;
+	}
+
+	private boolean getChildren(Widget parentWidget, String textOption)
+	{
+		Widget[] children = parentWidget.getStaticChildren();
+		if (children.length == 0)
+		{
+			return false;
+		}
+
+		int currentMin;
+		int currentMax;
+		if (max == -1 || min == -1)
+		{
+			currentMin = 0;
+			currentMax = children.length;
+		}
+		else
+		{
+			currentMax = Math.min(children.length, max);
+			currentMin = Math.max(0, min);
+		}
+
+		for (int i = currentMin; i < currentMax; i++)
+		{
+			Widget currentWidget = parentWidget.getStaticChildren()[i];
+			if (currentWidget.getNestedChildren() != null)
+			{
+				if (currentWidget.getText().contains(textOption))
+				{
+					return true;
+				}
+			}
+			else
+			{
+				if (getChildren(currentWidget, textOption))
+				{
+					return true;
 				}
 			}
 		}

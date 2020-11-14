7cf2663322b281ccb5e81e9edca35e245b1bb895
Zoinkwiz <zoinkwiz@hotmail.co.uk>
2020-11-14 17:40:19 +0000

    Fix for objects on different levels not correctly highlighting

--- src/main/java/com/questhelper/steps/ObjectStep.java
+++ src/main/java/com/questhelper/steps/ObjectStep.java
@@ -1,3 +1,27 @@
+/*
+ * Copyright (c) 2020, Zoinkwiz
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 package com.questhelper.steps;
 
 import com.questhelper.requirements.Requirement;
@@ -23,6 +47,7 @@ import net.runelite.api.events.GameObjectChanged;
 import net.runelite.api.events.GameObjectDespawned;
 import net.runelite.api.events.GameObjectSpawned;
 import net.runelite.api.events.GameStateChanged;
+import net.runelite.api.events.GameTick;
 import net.runelite.api.events.GroundObjectChanged;
 import net.runelite.api.events.GroundObjectDespawned;
 import net.runelite.api.events.GroundObjectSpawned;
@@ -35,6 +60,8 @@ import static com.questhelper.QuestHelperWorldOverlay.CLICKBOX_BORDER_COLOR;
 import static com.questhelper.QuestHelperWorldOverlay.CLICKBOX_FILL_COLOR;
 import static com.questhelper.QuestHelperWorldOverlay.CLICKBOX_HOVER_BORDER_COLOR;
 import com.questhelper.questhelpers.QuestHelper;
+import net.runelite.client.plugins.cluescrolls.clues.LocationsClueScroll;
+import net.runelite.client.plugins.cluescrolls.clues.ObjectClueScroll;
 import net.runelite.client.ui.overlay.OverlayUtil;
 
 public class ObjectStep extends DetailedQuestStep
@@ -66,35 +93,7 @@ public class ObjectStep extends DetailedQuestStep
 
 		if (worldPoint != null)
 		{
-			Collection<WorldPoint> localWorldPoints = toLocalInstance(client, worldPoint);
-			for (WorldPoint point : localWorldPoints)
-			{
-				LocalPoint localPoint = LocalPoint.fromWorld(client, point);
-				if (localPoint == null)
-				{
-					return;
-				}
-				Tile[][][] tiles = client.getScene().getTiles();
-				if (tiles == null)
-				{
-					return;
-				}
-				for (Tile[][] plane : tiles)
-				{
-					Tile tile = plane[localPoint.getSceneX()][localPoint.getSceneY()];
-					if (tile != null)
-					{
-						for (GameObject object : tile.getGameObjects())
-						{
-							handleObjects(object);
-						}
-
-						handleObjects(tile.getDecorativeObject());
-						handleObjects(tile.getGroundObject());
-						handleObjects(tile.getWallObject());
-					}
-				}
-			}
+			checkTileForObject(worldPoint);
 		}
 		else
 		{
@@ -115,6 +114,49 @@ public class ObjectStep extends DetailedQuestStep
 						handleObjects(tile.getWallObject());
 					}
 				}
+			}
+		}
+	}
+
+	@Subscribe
+	public void onGameTick(final GameTick event)
+	{
+		if (worldPoint == null)
+		{
+			return;
+		}
+		object = null;
+		checkTileForObject(worldPoint);
+	}
+
+	public void checkTileForObject(WorldPoint wp)
+	{
+		Collection<WorldPoint> localWorldPoints = toLocalInstance(client, wp);
+
+		for (WorldPoint point : localWorldPoints)
+		{
+			LocalPoint localPoint = LocalPoint.fromWorld(client, point);
+			if (localPoint == null)
+			{
+				continue;
+			}
+			Tile[][][] tiles = client.getScene().getTiles();
+			if (tiles == null)
+			{
+				continue;
+			}
+
+			Tile tile = tiles[client.getPlane()][localPoint.getSceneX()][localPoint.getSceneY()];
+			if (tile != null)
+			{
+				for (GameObject object : tile.getGameObjects())
+				{
+					handleObjects(object);
+				}
+
+				handleObjects(tile.getDecorativeObject());
+				handleObjects(tile.getGroundObject());
+				handleObjects(tile.getWallObject());
 			}
 		}
 	}

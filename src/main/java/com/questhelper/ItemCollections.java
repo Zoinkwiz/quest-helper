28e57b3eaec77349e9a453dc71d97840c6af1ec0
Haavardaw <72413703+Haavardaw@users.noreply.github.com>
2020-11-18 02:08:06 +0100

    Added logs for fire to itemcollections
    Added all different logs for fire to itemcollections

--- src/main/java/com/questhelper/ItemCollections.java
+++ src/main/java/com/questhelper/ItemCollections.java
@@ -380,6 +380,7 @@ public class ItemCollections
 			ItemID.DIGSITE_PENDANT_5
 	));
 
+<<<<<<< Updated upstream
 	@Getter
 	private static final List<Integer> nails = new ArrayList<>(Arrays.asList(
 		ItemID.BRONZE_NAILS,
@@ -430,5 +431,37 @@ public class ItemCollections
 		ItemID.ANCIENT_WYVERN_SHIELD_21634,
 		ItemID.ANTIDRAGON_SHIELD,
 		ItemID.ANTIDRAGON_SHIELD_8282
+=======
+
+	@Getter
+	private static final List<Integer> logsForFire = new ArrayList<>(Arrays.asList(
+			ItemID.REDWOOD_LOGS,
+			ItemID.MAGIC_LOGS,
+			ItemID.BLISTERWOOD_LOGS,
+			ItemID.YEW_LOGS,
+			ItemID.MAHOGANY_LOGS,
+			ItemID.MAPLE_LOGS,
+			ItemID.ARCTIC_PINE_LOGS,
+			ItemID.TEAK_LOGS,
+			ItemID.WILLOW_LOGS,
+			ItemID.OAK_LOGS,
+			ItemID.ACHEY_TREE_LOGS,
+			ItemID.LOGS,
+			ItemID.REDWOOD_PYRE_LOGS,
+			ItemID.MAGIC_PYRE_LOGS,
+			ItemID.YEW_PYRE_LOGS,
+			ItemID.MAHOGANY_PYRE_LOGS,
+			ItemID.MAPLE_PYRE_LOGS,
+			ItemID.ARCTIC_PYRE_LOGS,
+			ItemID.TEAK_PYRE_LOGS,
+			ItemID.WILLOW_PYRE_LOGS,
+			ItemID.OAK_PYRE_LOGS,
+			ItemID.PYRE_LOGS,
+			ItemID.GREEN_LOGS,
+			ItemID.RED_LOGS,
+			ItemID.PURPLE_LOGS,
+			ItemID.WHITE_LOGS,
+			ItemID.BLUE_LOGS
+>>>>>>> Stashed changes
 	));
 }

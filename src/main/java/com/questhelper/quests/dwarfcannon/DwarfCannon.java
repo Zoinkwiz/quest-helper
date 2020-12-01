493da263420d046e3ac3485e85db530e2603618c
Zoinkwiz <zoinkwiz@hotmail.co.uk>
2020-12-01 15:15:44 +0000

    Various fixes for Dwarf Cannon
    - Makes use of sub-steps to reduce noise in sidebar
    - Adds itemrequirements to steps where items are required
    Fix step 7 incorrectly telling you to talk to Lawgof too early
    - Added use of null objects where appropriate

--- src/main/java/com/questhelper/quests/dwarfcannon/DwarfCannon.java
+++ src/main/java/com/questhelper/quests/dwarfcannon/DwarfCannon.java
@@ -65,10 +65,8 @@ public class DwarfCannon extends BasicQuestHelper
 
 		steps.put(6, talkToCaptainLawgof4);
 
-		ConditionalStep fixUpCannon = new ConditionalStep(this, useToolkit);
-		fixUpCannon.addStep(new Conditions(safetyFixed, springFixed, cannonFixed), talkToCaptainLawgof5);
-		steps.put(7, fixUpCannon);
-		steps.put(8, fixUpCannon);
+		steps.put(7, useToolkit);
+		steps.put(8, talkToCaptainLawgof5);
 
 		//Ammo mould and back
 		ConditionalStep captainLawgofFinal = new ConditionalStep(this, talkToNulodion);
@@ -86,7 +84,9 @@ public class DwarfCannon extends BasicQuestHelper
 		teleToKand = new ItemRequirement("Teleport to Ardougne, Skills Necklace, or Games Necklace", -1);
 		hammer = new ItemRequirement("Hammer", ItemID.HAMMER);
 		railing = new ItemRequirement("Railing", ItemID.RAILING);
+		railing.setTip("You can get more from Captain Lawgof");
 		toolkit = new ItemRequirement("Toolkit", ItemID.TOOLKIT);
+		toolkit.setHighlightInInventory(true);
 		dwarfRemains = new ItemRequirement("Dwarf Remains", ItemID.DWARF_REMAINS);
 		cannonballMould = new ItemRequirement("Cannonball Mould", ItemID.AMMO_MOULD);
 		nulodionsNotes = new ItemRequirement("Nulodion's Notes", ItemID.NULODIONS_NOTES);
@@ -137,33 +137,35 @@ public class DwarfCannon extends BasicQuestHelper
 		talkToCaptainLawgof.addDialogStep("Sure, I'd be honoured to join.");
 
 		//Fix the 6 bent railings, these railings don't have different IDs from the normal railings
-		inspectRailings1 = new TileStep(this, new WorldPoint(2555, 3479, 0), "Inspect Railings to fix them.");
-		inspectRailings2 = new TileStep(this, new WorldPoint(2557, 3468, 0), "Inspect Railings to fix them.");
-		inspectRailings3 = new TileStep(this, new WorldPoint(2559, 3458, 0), "Inspect Railings to fix them.");
-		inspectRailings4 = new TileStep(this, new WorldPoint(2563, 3457, 0), "Inspect Railings to fix them.");
-		inspectRailings5 = new TileStep(this, new WorldPoint(2573, 3457, 0), "Inspect Railings to fix them.");
-		inspectRailings6 = new TileStep(this, new WorldPoint(2577, 3457, 0), "Inspect Railings to fix them.");
+		inspectRailings1 = new ObjectStep(this, NullObjectID.NULL_15590, new WorldPoint(2555, 3479, 0), "Inspect the 6 damaged railings around the  camp to fix them.", hammer, railing);
+		inspectRailings2 = new ObjectStep(this, NullObjectID.NULL_15591, new WorldPoint(2557, 3468, 0), "Inspect the railings to fix them.", hammer, railing);
+		inspectRailings3 = new ObjectStep(this, NullObjectID.NULL_15592, new WorldPoint(2559, 3458, 0), "Inspect the railings to fix them.", hammer, railing);
+		inspectRailings4 = new ObjectStep(this, NullObjectID.NULL_15593, new WorldPoint(2563, 3457, 0), "Inspect the railings to fix them.", hammer, railing);
+		inspectRailings5 = new ObjectStep(this, NullObjectID.NULL_15594, new WorldPoint(2573, 3457, 0), "Inspect the railings to fix them.", hammer, railing);
+		inspectRailings6 =new ObjectStep(this, NullObjectID.NULL_15595, new WorldPoint(2577, 3457, 0), "Inspect the railings to fix them.", hammer, railing);
+		inspectRailings1.addSubSteps(inspectRailings2, inspectRailings3, inspectRailings4, inspectRailings5, inspectRailings6);
 
 		//Get dwarf remains
 		talkToCaptainLawgof2 = new NpcStep(this, NpcID.CAPTAIN_LAWGOF, new WorldPoint(2567, 3460, 0), "Talk to Captain Lawgof again.  Make sure to complete the entire dialogue.");
-		gotoTower = new ObjectStep(this, ObjectID.LADDER_16683, new WorldPoint(2570, 3441, 0), "Go to the top floor of the tower south of Captain Lawgof.");
+		gotoTower = new ObjectStep(this, ObjectID.LADDER_16683, new WorldPoint(2570, 3441, 0), "Go to the top floor of the tower south of Captain Lawgof and get the remains there.");
 		goToTower2 = new ObjectStep(this, ObjectID.LADDER_11, new WorldPoint(2570, 3443, 1), "Go up the second ladder.");
 
-		//Highlight on dwarf remains isn't working, highlight tile is workaround
-		//getRemainsStep = new ObjectStep(this,ObjectID.DWARF_REMAINS,new WorldPoint(2567,3444,2),"Get the dwarf remains at the top of the tower.");
-		getRemainsStep = new TileStep(this, new WorldPoint(2567, 3444, 2), "Get the dwarf remains at the top of the tower.");
+		getRemainsStep = new ObjectStep(this, NullObjectID.NULL, new WorldPoint(2567, 3444, 2), "Get the dwarf remains at the top of the tower.");
+		gotoTower.addSubSteps(goToTower2, getRemainsStep);
 
 		downTower = new ObjectStep(this, ObjectID.LADDER_16679, new WorldPoint(2570, 3443, 2), "Go down the first ladder.");
 		downTower2 = new ObjectStep(this, ObjectID.LADDER_16679, new WorldPoint(2570, 3441, 1), "Go down the second ladder.");
 		talkToCaptainLawgof3 = new NpcStep(this, NpcID.CAPTAIN_LAWGOF, new WorldPoint(2567, 3460, 0), "Return the remains to Captain Lawgof.");
+		talkToCaptainLawgof3.addSubSteps(downTower, downTower2);
 
 		//Cave
 		gotoCave = new ObjectStep(this, ObjectID.CAVE_ENTRANCE, new WorldPoint(2624, 3393, 0), "Go to the cave entrance east of the Fishing Guild door.");
-		searchCrates = new ObjectStep(this, ObjectID.CRATE, new WorldPoint(2571, 9850, 0), "Search the crates in the North West corner to find Lollk.");
+		searchCrates = new ObjectStep(this, ObjectID.CRATE, new WorldPoint(2571, 9850, 0), "Search the crates in the north west corner to find Lollk.");
 		talkToCaptainLawgof4 = new NpcStep(this, NpcID.CAPTAIN_LAWGOF, new WorldPoint(2567, 3460, 0), "Return to Captain Lawgof.");
 		talkToCaptainLawgof4.addDialogStep("Okay, I'll see what I can do.");
 
 		//Fix cannon
+		// TODO: Update this to highlight widgets as you progress, indicating what tool to use on what
 		useToolkit = new ObjectStep(this, NullObjectID.NULL_15597, new WorldPoint(2563, 3462, 0), "Use the toolkit on the broken multicannon.  Use the right tool on the spring, the middle tool on the Safety switch, and the left ool on the gear.");
 		useToolkit.addIcon(ItemID.TOOLKIT);
 		talkToCaptainLawgof5 = new NpcStep(this, NpcID.CAPTAIN_LAWGOF, new WorldPoint(2567, 3460, 0), "Talk to Captain Lawgof (There will be a short pause in dialogue.  Both need to be completed.).");
@@ -171,7 +173,7 @@ public class DwarfCannon extends BasicQuestHelper
 
 		//Cannonball mould
 		talkToNulodion = new NpcStep(this, NpcID.NULODION, new WorldPoint(3012, 3453, 0), "Go talk to Nulodion at the Dwarven Black Guard camp (north-east of Falador, South of Ice Mountain).");
-		talkToCaptainLawgof6 = new NpcStep(this, NpcID.CAPTAIN_LAWGOF, new WorldPoint(2567, 3460, 0), "Finally, return to Captain Lawgof with the ammo mould and Nulodion's Notes.");
+		talkToCaptainLawgof6 = new NpcStep(this, NpcID.CAPTAIN_LAWGOF, new WorldPoint(2567, 3460, 0), "Finally, return to Captain Lawgof with the ammo mould and Nulodion's Notes.", nulodionsNotes, cannonballMould);
 	}
 
 	@Override
@@ -189,7 +191,7 @@ public class DwarfCannon extends BasicQuestHelper
 	{
 		ArrayList<PanelDetails> allSteps = new ArrayList<>();
 		allSteps.add(new PanelDetails("Starting off", new ArrayList<>(Arrays.asList(talkToCaptainLawgof))));
-		allSteps.add(new PanelDetails("Repair and Retrieval", new ArrayList<>(Arrays.asList(inspectRailings1, inspectRailings2, inspectRailings3, inspectRailings4, inspectRailings5, inspectRailings6, talkToCaptainLawgof2, gotoTower, goToTower2, getRemainsStep, downTower, downTower2, talkToCaptainLawgof3))));
+		allSteps.add(new PanelDetails("Repair and Retrieval", new ArrayList<>(Arrays.asList(inspectRailings1, talkToCaptainLawgof2, gotoTower, talkToCaptainLawgof3))));
 		allSteps.add(new PanelDetails("Find Lollk and Fix Cannon", new ArrayList<>(Arrays.asList(gotoCave, searchCrates, talkToCaptainLawgof4, useToolkit, talkToCaptainLawgof5))));
 		allSteps.add(new PanelDetails("Get Ammo Mould", new ArrayList<>(Arrays.asList(talkToNulodion, talkToCaptainLawgof6))));
 		return allSteps;

b442c555826aa2a4012e8746e8e3245604da4509
RobertDIversen <RobertDIversen@gmail.com>
2020-12-23 12:04:32 +1300

    Add Tower of Life quest.Add generic PuzzleStep

---
+++ src/main/java/com/questhelper/steps/PuzzleStep.java
@@ -0,0 +1,68 @@
+package com.questhelper.steps;
+
+import com.questhelper.QuestHelperPlugin;
+import com.questhelper.questhelpers.QuestHelper;
+import com.questhelper.requirements.Requirement;
+import net.runelite.api.events.GameTick;
+import net.runelite.api.widgets.Widget;
+import net.runelite.client.eventbus.Subscribe;
+
+import java.awt.*;
+import java.util.HashSet;
+public class PuzzleStep extends DetailedQuestStep{
+
+    ButtonHighlighCalculator highlightCalculator;
+    private HashSet<WidgetDetails> highlightedButtons = new HashSet<>();
+
+    public PuzzleStep(QuestHelper questHelper, ButtonHighlighCalculator highlightCalculator, Requirement... requirements)
+    {
+        this(questHelper, "Click the highlighted buttons to complete the puzzle", highlightCalculator, requirements);
+    }
+
+    public PuzzleStep(QuestHelper questHelper, String text, ButtonHighlighCalculator highlightCalculator, Requirement... requirements)
+    {
+        super(questHelper, text, requirements);
+        this.highlightCalculator = highlightCalculator;
+    }
+
+    @Override
+    public void startUp()
+    {
+        this.highlightedButtons = highlightCalculator.getHighlightedButtons();
+    }
+
+    @Subscribe
+    public void onGameTick(GameTick event)
+    {
+        this.highlightedButtons = highlightCalculator.getHighlightedButtons();
+    }
+
+    @Override
+    public void makeWidgetOverlayHint(Graphics2D graphics, QuestHelperPlugin plugin)
+    {
+        super.makeWidgetOverlayHint(graphics, plugin);
+        for (WidgetDetails button : highlightedButtons)
+        {
+            if (button == null)
+            {
+                continue;
+            }
+
+            Widget widget = client.getWidget(button.getGroupID(), button.getChildID());
+            if (widget != null)
+            {
+                graphics.setColor(new Color(0, 255, 255, 65));
+                graphics.fill(widget.getBounds());
+                graphics.setColor(Color.CYAN);
+                graphics.draw(widget.getBounds());
+            }
+
+        }
+    }
+
+
+    public interface ButtonHighlighCalculator
+    {
+        HashSet<WidgetDetails> getHighlightedButtons();
+    }
+}

cd019e0841bb5f397760c69269e0c9bb0e79ed9a
Senmori <thesenmori@gmail.com>
2021-01-13 01:13:16 -0500

    Add QuestRequirement to allow for specifying a quest that should in a certain QuestState.

---
+++ src/main/java/com/questhelper/requirements/QuestRequirement.java
@@ -0,0 +1,49 @@
+package com.questhelper.requirements;
+
+import java.awt.Color;
+import java.util.Locale;
+import lombok.Getter;
+import net.runelite.api.Client;
+import net.runelite.api.Quest;
+import net.runelite.api.QuestState;
+
+@Getter
+public class QuestRequirement extends Requirement
+{
+	private Quest quest;
+	private QuestState requiredState;
+	private String displayText = null;
+
+	public QuestRequirement(Quest quest, QuestState requiredState) {
+		this.quest = quest;
+		this.requiredState = requiredState;
+	}
+
+	public QuestRequirement(Quest quest, QuestState requiredState, String displayText) {
+		this(quest, requiredState);
+		this.displayText = displayText;
+	}
+
+	@Override
+	public Color getColor(Client client)
+	{
+		return check(client) ? Color.GREEN : Color.RED;
+	}
+
+	@Override
+	public boolean check(Client client)
+	{
+		QuestState state = quest.getState(client);
+		if (requiredState == QuestState.IN_PROGRESS && state == QuestState.FINISHED) {
+			return true;
+		}
+		return state == requiredState;
+	}
+
+	@Override
+	public String getDisplayText()
+	{
+		String text = Character.toUpperCase(requiredState.name().charAt(0)) + requiredState.name().substring(1);
+		return text.replaceAll("_", " ") + " " + quest.getName();
+	}
+}

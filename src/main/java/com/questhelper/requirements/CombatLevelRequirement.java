11322a0d6cc0fe7f2f0d4b0ba2e97b7430bcdf5c
Senmori <thesenmori@gmail.com>
2021-01-13 02:00:56 -0500

    Add CombatLevel Requirement and FavourRequirement so requirements can dictate a certain combat level and kourend favour.

---
+++ src/main/java/com/questhelper/requirements/CombatLevelRequirement.java
@@ -0,0 +1,31 @@
+package com.questhelper.requirements;
+
+import lombok.RequiredArgsConstructor;
+import net.runelite.api.Client;
+import net.runelite.api.Experience;
+import net.runelite.api.Skill;
+@RequiredArgsConstructor
+public class CombatLevelRequirement extends Requirement
+{
+	private final int level;
+
+	@Override
+	public boolean check(Client client)
+	{
+		int combatLevel = Experience.getCombatLevel(
+			client.getRealSkillLevel(Skill.ATTACK),
+			client.getRealSkillLevel(Skill.STRENGTH),
+			client.getRealSkillLevel(Skill.DEFENCE),
+			client.getRealSkillLevel(Skill.HITPOINTS),
+			client.getRealSkillLevel(Skill.MAGIC),
+			client.getRealSkillLevel(Skill.RANGED),
+			client.getRealSkillLevel(Skill.PRAYER));
+		return combatLevel >= level;
+	}
+
+	@Override
+	public String getDisplayText()
+	{
+		return "Combat Level " + level;
+	}
+}

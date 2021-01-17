4dce4394ee8233ac94b97ef17ec76eceffda7d77
Senmori <thesenmori@gmail.com>
2021-01-17 15:35:02 -0500

    Add javadocs for better clarification

--- src/main/java/com/questhelper/questhelpers/QuestDebugRenderer.java
+++ src/main/java/com/questhelper/questhelpers/QuestDebugRenderer.java
@@ -33,9 +33,12 @@ import net.runelite.client.ui.overlay.components.PanelComponent;
 
 public interface QuestDebugRenderer
 {
+	/** Used to render overlay's like quest steps. */
 	default void renderDebugOverlay(Graphics graphics, QuestHelperPlugin plugin, QuestHelper quest, PanelComponent panelComponent) {}
 
+	/** Used to render an overlay similar to how fishing spot overlays work. */
 	default void renderDebugWorldOverlayHint(Graphics2D graphics, QuestHelperPlugin plugin, QuestHelper quest, PanelComponent panelComponent) {}
 
+	/** Used to render overlays on widgets */
 	default void renderDebugWidgetOverlayHint(Graphics2D graphics, QuestHelperPlugin plugin, QuestHelper quest, PanelComponent panelComponent) {}
 }

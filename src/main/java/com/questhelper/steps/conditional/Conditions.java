629bb2f0218b82565bbe81010cf4f5e0128db22f
Senmori <thesenmori@gmail.com>
2021-01-24 01:01:10 -0500

    Remove extra TODO

--- src/main/java/com/questhelper/steps/conditional/Conditions.java
+++ src/main/java/com/questhelper/steps/conditional/Conditions.java
@@ -108,7 +108,6 @@ public class Conditions extends ConditionForStep
 		}
 
 		//TODO: Replace with LogicType check, however more testing to be done to make sure nothing breaks
-		//TODO: Replace with LogicType check, however more testing to be done to make sure nothing breaks
 		if ((conditionsPassed > 0 && logicType == LogicType.OR)
 			|| (conditionsPassed == 0 && logicType == LogicType.NOR)
 			|| (conditionsPassed == conditions.size() && logicType == LogicType.AND)

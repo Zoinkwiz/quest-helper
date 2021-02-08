1f7e421dcaa01bafd80e1dccf87ae00b87217114
Senmori <thesenmori@gmail.com>
2021-02-08 15:00:22 -0500

    Move copyrights back to their original owners.

--- src/main/java/com/questhelper/requirements/item/FollowerItemRequirement.java
+++ src/main/java/com/questhelper/requirements/item/FollowerItemRequirement.java
@@ -1,6 +1,6 @@
 /*
  *
- *  * Copyright (c) 2021, Senmori
+ *  * Copyright (c) 2021, Zoinkwiz <https://github.com/Zoinkwiz>
  *  * All rights reserved.
  *  *
  *  * Redistribution and use in source and binary forms, with or without

0c70c6d4afaf5502a30450f259917366744a8627
Grandpa Seth <51179552+nnapolitano@users.noreply.github.com>
2021-02-11 04:38:18 -0500

    Arrow Toggle (#207)
    * added toggle for minimap arrows

--- src/main/java/com/questhelper/QuestHelperConfig.java
+++ src/main/java/com/questhelper/QuestHelperConfig.java
@@ -128,6 +128,16 @@ public interface QuestHelperConfig extends Config
 		return true;
 	}
 
+	@ConfigItem(
+			keyName = "showMiniMapArrow",
+			name = "Display arrows on the mini-map and overworld",
+			description = "Choose whether flashing arrows point to the next objective"
+	)
+	default boolean showMiniMapArrow()
+	{
+		return true;
+	}
+	
 	@ConfigSection(
 		position = 1,
 		name = "Colors",

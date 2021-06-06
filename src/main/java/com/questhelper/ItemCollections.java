8e12d19e4ad8a9ee7c4b0ac2a14c380a199b2b8e
Mark <mark.obasill@gmail.com>
2021-06-06 05:02:30 -0400

    Added the fremennik diary + a special attack requirement, originally for use in the dagannoth cave(for rune thrownaxes)

--- src/main/java/com/questhelper/ItemCollections.java
+++ src/main/java/com/questhelper/ItemCollections.java
@@ -1110,4 +1110,29 @@ public class ItemCollections
 		ItemID.RAW_ANGLERFISH,
 		ItemID.RAW_DARK_CRAB
 	);
+
+	@Getter
+	private static final List<Integer> fairyStaff = ImmutableList.of(
+			ItemID.LUNAR_STAFF,
+			ItemID.LUNAR_STAFF__PT3,
+			ItemID.LUNAR_STAFF__PT2,
+			ItemID.LUNAR_STAFF__PT1,
+			ItemID.DRAMEN_STAFF
+	);
+
+	@Getter
+	private static final List<Integer> crossbows = ImmutableList.of(
+			ItemID.BRONZE_CROSSBOW,
+			ItemID.BLURITE_CROSSBOW,
+			ItemID.IRON_CROSSBOW,
+			ItemID.STEEL_CROSSBOW,
+			ItemID.MITHRIL_CROSSBOW,
+			ItemID.ADAMANT_CROSSBOW,
+			ItemID.RUNE_CROSSBOW,
+			ItemID.DRAGON_CROSSBOW,
+			ItemID.CROSSBOW,
+			ItemID.DORGESHUUN_CROSSBOW,
+			ItemID.ARMADYL_CROSSBOW,
+			ItemID.DRAGON_HUNTER_CROSSBOW
+	);
 }

dc566afa81c098b3ae01b123a0ccdee2c97dc942
Riley Schroeder <riley.schroeder@lakeshore.com>
2021-09-15 22:28:57 -0400

    Add kourend diary stubs

---
+++ src/main/java/com/questhelper/achievementdiaries/kourend/KourendEasy.java
@@ -0,0 +1,97 @@
+/*
+ * Copyright (c) 2021, Kerpackie <https://github.com/Kerpackie/>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package com.questhelper.achievementdiaries.kourend;
+
+import com.questhelper.QuestDescriptor;
+import com.questhelper.QuestHelperQuest;
+import com.questhelper.panel.PanelDetails;
+import com.questhelper.questhelpers.ComplexStateQuestHelper;
+import com.questhelper.requirements.Requirement;
+import com.questhelper.requirements.item.ItemRequirement;
+import com.questhelper.steps.QuestStep;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@QuestDescriptor(
+    quest = QuestHelperQuest.KOUREND_EASY
+)
+
+public class KourendEasy extends ComplexStateQuestHelper
+{
+    @Override
+    public QuestStep loadStep()
+    {
+        return null;
+    }
+
+    public void setupRequirements()
+    {
+
+    }
+
+    public void loadZones()
+    {
+
+    }
+
+    public void setupSteps()
+    {
+
+    }
+
+    @Override
+    public List<ItemRequirement> getItemRequirements()
+    {
+        return null;
+    }
+
+    @Override
+    public List<ItemRequirement> getItemRecommended()
+    {
+        return null;
+    }
+
+    @Override
+    public List<Requirement> getGeneralRequirements()
+    {
+        ArrayList<Requirement> req = new ArrayList<>();
+
+        // TODO: add reqs
+
+        return req;
+    }
+
+    @Override
+    public List<PanelDetails> getPanels()
+    {
+        List<PanelDetails> allSteps = new ArrayList<>();
+
+        // TODO: add steps
+
+        return allSteps;
+    }
+}

f4bf84346c9afdfe556a14eb644e04fac8b52a68
Kerpackie <81828282+Kerpackie@users.noreply.github.com>
2021-09-18 21:18:55 +0100

    Update Quest Reward (E)

--- src/main/java/com/questhelper/quests/elementalworkshopi/ElementalWorkshopI.java
+++ src/main/java/com/questhelper/quests/elementalworkshopi/ElementalWorkshopI.java
@@ -42,6 +42,10 @@ import com.questhelper.requirements.ZoneRequirement;
 import com.questhelper.requirements.conditional.Conditions;
 import com.questhelper.requirements.conditional.NpcCondition;
 import com.questhelper.requirements.util.LogicType;
+import com.questhelper.rewards.ExperienceReward;
+import com.questhelper.rewards.ItemReward;
+import com.questhelper.rewards.QuestPointReward;
+import com.questhelper.rewards.UnlockReward;
 import com.questhelper.steps.ConditionalStep;
 import com.questhelper.steps.DetailedQuestStep;
 import com.questhelper.steps.ItemStep;
@@ -306,6 +310,34 @@ public class ElementalWorkshopI extends ComplexStateQuestHelper
 	public List<String> getCombatRequirements()
 	{
 		return Collections.singletonList("Earth elemental (level 35)");
+	}
+
+	@Override
+	public QuestPointReward getQuestPointReward()
+	{
+		return new QuestPointReward(1);
+	}
+
+	@Override
+	public List<ExperienceReward> getExperienceRewards()
+	{
+		return  Arrays.asList(
+				new ExperienceReward(Skill.CRAFTING, 5000),
+				new ExperienceReward(Skill.SMITHING, 5000));
+	}
+
+	@Override
+	public List<ItemReward> getItemRewards()
+	{
+		return Collections.singletonList(new ItemReward("An Elemental Shield", ItemID.ELEMENTAL_SHIELD, 1));
+	}
+
+	@Override
+	public List<UnlockReward> getUnlockRewards()
+	{
+		return Arrays.asList(
+				new UnlockReward("Access to the Elemental Workshop."),
+				new UnlockReward("Ability to craft and wield Elemental equipment."));
 	}
 
 	@Override

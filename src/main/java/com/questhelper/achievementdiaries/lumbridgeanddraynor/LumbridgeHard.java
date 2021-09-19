7a12faa814fb62246c9675ec6b6b4e8b13e635de
Osball <mark.obasill@gmail.com>
2021-09-19 19:14:49 -0400

    adding madeAmuletU

--- src/main/java/com/questhelper/achievementdiaries/lumbridgeanddraynor/LumbridgeHard.java
+++ src/main/java/com/questhelper/achievementdiaries/lumbridgeanddraynor/LumbridgeHard.java
@@ -198,6 +198,7 @@ public class LumbridgeHard extends ComplexStateQuestHelper
 			)
 		);
 
+
 		recipeForDisaster = new QuestRequirement(QuestHelperQuest.RECIPE_FOR_DISASTER, QuestState.FINISHED);
 		anotherSliceOfHAM = new QuestRequirement(QuestHelperQuest.ANOTHER_SLICE_OF_HAM, QuestState.FINISHED);
 		tearOfGuth = new QuestRequirement(QuestHelperQuest.TEARS_OF_GUTHIX, QuestState.FINISHED);

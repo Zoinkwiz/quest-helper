93b34f715dcd785b5c31622c24fa8c83073ee418
Osball <mark.obasill@gmail.com>
2021-11-07 04:20:03 -0500

    Initial configuration of files, completion of easy, and fixed varbits / quest orders

--- src/main/java/com/questhelper/ItemCollections.java
+++ src/main/java/com/questhelper/ItemCollections.java
@@ -1221,10 +1221,10 @@ public class ItemCollections
 
 	@Getter
 	private static final List<Integer> compost = ImmutableList.of(
-			ItemID.BOTTOMLESS_COMPOST_BUCKET_22997,
-			ItemID.ULTRACOMPOST,
-			ItemID.SUPERCOMPOST,
-			ItemID.COMPOST
+		ItemID.BOTTOMLESS_COMPOST_BUCKET_22997,
+		ItemID.ULTRACOMPOST,
+		ItemID.SUPERCOMPOST,
+		ItemID.COMPOST
 	);
 
 	@Getter
@@ -1265,7 +1265,7 @@ public class ItemCollections
 	);
 
 	@Getter
-	private  static final List<Integer> cookingGuild = ImmutableList.of(
+	private static final List<Integer> cookingGuild = ImmutableList.of(
 		ItemID.CHEFS_HAT,
 		ItemID.VARROCK_ARMOUR_3,
 		ItemID.VARROCK_ARMOUR_4,
@@ -1312,7 +1312,7 @@ public class ItemCollections
 		ItemID.SLAYER_GLOVES,
 		ItemID.VOID_KNIGHT_GLOVES,
 		ItemID.WHITE_GLOVES
-		);
+	);
 
 	@Getter
 	private static final List<Integer> questCape = ImmutableList.of(
@@ -1367,6 +1367,66 @@ public class ItemCollections
 		ItemID.ELEMENTAL_TALISMAN,
 		ItemID.FIRE_TIARA,
 		ItemID.FIRE_TALISMAN
+	);
+
+	@Getter
+	private static final List<Integer> teamCape = ImmutableList.of(
+		ItemID.TEAM1_CAPE,
+		ItemID.TEAM2_CAPE,
+		ItemID.TEAM3_CAPE,
+		ItemID.TEAM4_CAPE,
+		ItemID.TEAM5_CAPE,
+		ItemID.TEAM6_CAPE,
+		ItemID.TEAM7_CAPE,
+		ItemID.TEAM8_CAPE,
+		ItemID.TEAM9_CAPE,
+		ItemID.TEAM10_CAPE,
+		ItemID.TEAM11_CAPE,
+		ItemID.TEAM12_CAPE,
+		ItemID.TEAM13_CAPE,
+		ItemID.TEAM14_CAPE,
+		ItemID.TEAM15_CAPE,
+		ItemID.TEAM16_CAPE,
+		ItemID.TEAM17_CAPE,
+		ItemID.TEAM18_CAPE,
+		ItemID.TEAM19_CAPE,
+		ItemID.TEAM20_CAPE,
+		ItemID.TEAM21_CAPE,
+		ItemID.TEAM22_CAPE,
+		ItemID.TEAM23_CAPE,
+		ItemID.TEAM24_CAPE,
+		ItemID.TEAM25_CAPE,
+		ItemID.TEAM26_CAPE,
+		ItemID.TEAM27_CAPE,
+		ItemID.TEAM28_CAPE,
+		ItemID.TEAM29_CAPE,
+		ItemID.TEAM30_CAPE,
+		ItemID.TEAM31_CAPE,
+		ItemID.TEAM32_CAPE,
+		ItemID.TEAM33_CAPE,
+		ItemID.TEAM34_CAPE,
+		ItemID.TEAM35_CAPE,
+		ItemID.TEAM36_CAPE,
+		ItemID.TEAM37_CAPE,
+		ItemID.TEAM38_CAPE,
+		ItemID.TEAM39_CAPE,
+		ItemID.TEAM40_CAPE,
+		ItemID.TEAM41_CAPE,
+		ItemID.TEAM42_CAPE,
+		ItemID.TEAM43_CAPE,
+		ItemID.TEAM44_CAPE,
+		ItemID.TEAM45_CAPE,
+		ItemID.TEAM46_CAPE,
+		ItemID.TEAM47_CAPE,
+		ItemID.TEAM48_CAPE,
+		ItemID.TEAM49_CAPE,
+		ItemID.TEAM50_CAPE
+	);
+
+	@Getter
+	private static final List<Integer> chaosAltar = ImmutableList.of(
+		ItemID.CHAOS_TIARA,
+		ItemID.CHAOS_TALISMAN
 	);
 
 }
---
+++ src/main/java/com/questhelper/achievementdiaries/wilderness/WildernessHard.java
@@ -0,0 +1,164 @@
+/*
+ * Copyright (c) 2021, Obasill <https://github.com/Obasill>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.questhelper.achievementdiaries.wilderness;
+
+import com.questhelper.ItemCollections;
+import com.questhelper.QuestHelperQuest;
+import com.questhelper.QuestVarPlayer;
+import com.questhelper.Zone;
+import com.questhelper.banktab.BankSlotIcons;
+import com.questhelper.questhelpers.ComplexStateQuestHelper;
+import com.questhelper.requirements.Requirement;
+import com.questhelper.requirements.ZoneRequirement;
+import com.questhelper.requirements.conditional.Conditions;
+import com.questhelper.requirements.player.CombatLevelRequirement;
+import com.questhelper.requirements.player.SkillRequirement;
+import com.questhelper.requirements.quest.QuestRequirement;
+import com.questhelper.requirements.util.LogicType;
+import com.questhelper.requirements.util.Operation;
+import com.questhelper.requirements.var.VarbitRequirement;
+import com.questhelper.requirements.var.VarplayerRequirement;
+import com.questhelper.steps.ConditionalStep;
+import com.questhelper.steps.DetailedQuestStep;
+import com.questhelper.steps.NpcStep;
+import com.questhelper.steps.ObjectStep;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import net.runelite.api.ItemID;
+import net.runelite.api.NpcID;
+import net.runelite.api.NullObjectID;
+import net.runelite.api.ObjectID;
+import net.runelite.api.QuestState;
+import net.runelite.api.Skill;
+import net.runelite.api.coords.WorldPoint;
+import com.questhelper.requirements.item.ItemRequirement;
+import com.questhelper.QuestDescriptor;
+import com.questhelper.panel.PanelDetails;
+import com.questhelper.steps.QuestStep;
+
+@QuestDescriptor(
+	quest = QuestHelperQuest.WILDERNESS_HARD
+)
+public class WildernessHard extends ComplexStateQuestHelper
+{
+	// Items required
+	ItemRequirement combatGear;
+
+	// Items recommended
+	ItemRequirement food;
+
+	// Quests required
+	Requirement taiBwoWannaiTrio;
+
+	Requirement not;
+
+	QuestStep claimReward;
+
+	Zone cave;
+
+	ZoneRequirement inCave;
+
+	@Override
+	public QuestStep loadStep()
+	{
+		loadZones();
+		setupRequirements();
+		setupSteps();
+
+		ConditionalStep doHard = new ConditionalStep(this, claimReward);
+		// doHard.addStep(notUsedShortcut, useShortcut);
+
+		return doHard;
+	}
+
+	public void setupRequirements()
+	{
+		not = new VarplayerRequirement(3600, false, 1);
+
+		combatGear = new ItemRequirement("Combat gear", -1, -1);
+		combatGear.setDisplayItemId(BankSlotIcons.getCombatGear());
+
+		food = new ItemRequirement("Food", ItemCollections.getGoodEatingFood(), -1);
+
+		inCave = new ZoneRequirement(cave);
+
+		taiBwoWannaiTrio = new QuestRequirement(QuestHelperQuest.TAI_BWO_WANNAI_TRIO, QuestState.FINISHED);
+	}
+
+	public void loadZones()
+	{
+		cave = new Zone(new WorldPoint(2821, 9545, 0), new WorldPoint(2879, 9663, 0));
+	}
+
+	public void setupSteps()
+	{
+
+		claimReward = new NpcStep(this, NpcID.LESSER_FANATIC, new WorldPoint(3121, 3518, 0),
+			"Talk to Lesser Fanatic in Edgeville to claim your reward!");
+		claimReward.addDialogStep("I have a question about my Achievement Diary.");
+	}
+
+	@Override
+	public List<ItemRequirement> getItemRequirements()
+	{
+		return Arrays.asList(combatGear);
+	}
+
+	@Override
+	public List<ItemRequirement> getItemRecommended()
+	{
+		return Arrays.asList(food);
+	}
+
+	@Override
+	public List<Requirement> getGeneralRequirements()
+	{
+		List<Requirement> reqs = new ArrayList<>();
+		reqs.add(new CombatLevelRequirement(100));
+		reqs.add(new SkillRequirement(Skill.AGILITY, 53));
+
+		reqs.add(taiBwoWannaiTrio);
+
+		return reqs;
+	}
+
+	@Override
+	public List<String> getCombatRequirements()
+	{
+		return Collections.singletonList("At least 1 Gorak (level 145)");
+	}
+
+	@Override
+	public List<PanelDetails> getPanels()
+	{
+		List<PanelDetails> allSteps = new ArrayList<>();
+
+		allSteps.add(new PanelDetails("Finishing off", Collections.singletonList(claimReward)));
+
+		return allSteps;
+	}
+}

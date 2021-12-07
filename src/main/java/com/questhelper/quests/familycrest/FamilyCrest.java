4665c6121985f3fbcbf70e71fdda3fead79435ad
Zoinkwiz <zoinkwiz@hotmail.co.uk>
2021-12-07 11:20:49 +0000

    Remove unused substep in Family Crest

--- src/main/java/com/questhelper/quests/familycrest/FamilyCrest.java
+++ src/main/java/com/questhelper/quests/familycrest/FamilyCrest.java
@@ -234,7 +234,6 @@ public class FamilyCrest extends BasicQuestHelper
 		talkToCalebOnceMore = new NpcStep(this, NpcID.CALEB, new WorldPoint(2819, 3452, 0), "Talk to Caleb in " +
 			"Catherby once more.");
 		talkToCalebOnceMore.addDialogStep("Uh.. what happened to the rest of the crest?");
-		talkToCalebWithFish.addSubSteps(talkToCalebOnceMore);
 
 		talkToGemTrader = new NpcStep(this, NpcID.GEM_TRADER, new WorldPoint(3286, 3211, 0), "Talk to the Gem Trader in Al Kharid.");
 		talkToGemTrader.addDialogStep("I'm in search of a man named Avan Fitzharmon.");

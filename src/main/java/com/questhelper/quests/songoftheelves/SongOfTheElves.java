08e7ff8faf137517cffe8d74e240403296692c2a
Zoinkwiz <zoinkwiz@hotmail.co.uk>
2021-12-11 22:18:48 +0000

    Fix elder cad vial check in SotE
    fixes #603

--- src/main/java/com/questhelper/quests/songoftheelves/SongOfTheElves.java
+++ src/main/java/com/questhelper/quests/songoftheelves/SongOfTheElves.java
@@ -335,7 +335,7 @@ public class SongOfTheElves extends BasicQuestHelper
 
 		ConditionalStep makePotion = new ConditionalStep(this, talkToElenaAfterTra);
 		makePotion.addStep(new Conditions(inversionPotion), talkToArianwynAfterPotion);
-		makePotion.addStep(new Conditions(crystalDust, cadantineSeed), useDustOnVial);
+		makePotion.addStep(new Conditions(crystalDust, elderCadantineVial), useDustOnVial);
 		makePotion.addStep(new Conditions(crystalDust, elderCadantine), useCadantineOnVial);
 		makePotion.addStep(new Conditions(crystal, elderCadantine), usePestleOnCrystal);
 		makePotion.addStep(elderCadantine, talkToArianwynAfterTra);

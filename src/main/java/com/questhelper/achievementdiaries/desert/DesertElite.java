209cc268f8a1084fccfcccdb2b216596457a045d
Jonathas Eloi <jonathaseloi@gmail.com>
2022-01-13 17:45:35 -0300

    Update DesertElite.java
    Duplicate requirement

--- src/main/java/com/questhelper/achievementdiaries/desert/DesertElite.java
+++ src/main/java/com/questhelper/achievementdiaries/desert/DesertElite.java
@@ -256,7 +256,6 @@ public class DesertElite extends ComplexStateQuestHelper
 	public List<Requirement> getGeneralRequirements()
 	{
 		List<Requirement> reqs = new ArrayList<>();
-		reqs.add(new SkillRequirement(Skill.CONSTRUCTION, 78));
 		reqs.add(new SkillRequirement(Skill.COOKING, 85));
 		reqs.add(new SkillRequirement(Skill.FLETCHING, 95));
 		reqs.add(new SkillRequirement(Skill.CONSTRUCTION, 78));

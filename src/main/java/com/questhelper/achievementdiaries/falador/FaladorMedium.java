98eda29a2952f109be886068777312e4962f620e
Felanbird <41973452+Felanbird@users.noreply.github.com>
2022-02-23 04:36:11 -0500

    Add alternate fishingExplosive to FaladorMedium.java

--- src/main/java/com/questhelper/achievementdiaries/falador/FaladorMedium.java
+++ src/main/java/com/questhelper/achievementdiaries/falador/FaladorMedium.java
@@ -163,6 +163,7 @@ public class FaladorMedium extends ComplexStateQuestHelper
 		emptySack = new ItemRequirement("Empty Sack", ItemID.EMPTY_SACK).showConditioned(notPlacedScarecrow);
 		rake = new ItemRequirement("Rake", ItemID.RAKE).showConditioned(notPlacedScarecrow);
 		fishingExplosive = new ItemRequirement("Fishing Explosive", ItemID.FISHING_EXPLOSIVE).showConditioned(notKilledMogre);
+		fishingExplosive.addAlternates(ItemID.FISHING_EXPLOSIVE_6664);
 		combatGear = new ItemRequirement("Combat Gear", -1, -1).showConditioned(notKilledMogre);
 		mithGrapple = new ItemRequirement("Mith grapple", ItemID.MITH_GRAPPLE_9419).showConditioned(notGrappleNorthWall);
 		anyCrossbow = new ItemRequirement("Any usable crossbow", ItemCollections.getCrossbows()).showConditioned(notGrappleNorthWall);

316ab018bc5082f56d4cc5e954d8b296868a1c4d
Jake <32685561+jacoblbeck@users.noreply.github.com>
2022-04-18 14:23:54 -0500

    update quest point total

--- src/main/java/com/questhelper/achievementdiaries/lumbridgeanddraynor/LumbridgeElite.java
+++ src/main/java/com/questhelper/achievementdiaries/lumbridgeanddraynor/LumbridgeElite.java
@@ -121,7 +121,7 @@ public class LumbridgeElite extends ComplexStateQuestHelper
 		notQCEmote = new VarplayerRequirement(1195, true, 9);
 
 		// todo find better way to check for all quests completed
-		allQuests = new QuestPointRequirement(286, Operation.EQUAL);
+		allQuests = new QuestPointRequirement(287, Operation.EQUAL);
 
 		lockpick = new ItemRequirement("Lockpick", ItemID.LOCKPICK).showConditioned(notRichChest);
 		crossbow = new ItemRequirement("Crossbow", ItemCollections.getCrossbows()).showConditioned(notMovario);

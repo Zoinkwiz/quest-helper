f52597057395cd42c2ec40c17253822fff0b0ecd
Alex Jurkiewicz <alex@jurkiewi.cz>
2022-11-22 22:54:55 +1100

    Specify which fungus is useful for safespotting the Tree Spirit (#968)
    Specify which fungus is useful for safespotting the Tree Spirit

--- src/main/java/com/questhelper/quests/lostcity/LostCity.java
+++ src/main/java/com/questhelper/quests/lostcity/LostCity.java
@@ -162,7 +162,7 @@ public class LostCity extends BasicQuestHelper
 		getAxe = new DetailedQuestStep(this, new WorldPoint(2843, 9760, 0), "Kill zombies until one drops a bronze axe.");
 		pickupAxe = new DetailedQuestStep(this, "Pick up the bronze axe", bronzeAxe);
 		attemptToCutDramen = new ObjectStep(this, ObjectID.DRAMEN_TREE, new WorldPoint(2861, 9735, 0), "Attempt to cut a branch from the Dramen tree. Be prepared for a Tree Spirit (level 101) to appear, which you can safespot behind nearby fungus.", bronzeAxe);
-		killDramenSpirit = new NpcStep(this, NpcID.TREE_SPIRIT, new WorldPoint(2859, 9734, 0), "Kill the Tree Spirit. They can be safespotted behind the nearby fungus.");
+		killDramenSpirit = new NpcStep(this, NpcID.TREE_SPIRIT, new WorldPoint(2859, 9734, 0), "Kill the Tree Spirit. They can be safespotted behind nearby fungi to the east.");
 		cutDramenBranch = new ObjectStep(this, ObjectID.DRAMEN_TREE, new WorldPoint(2861, 9735, 0), "Cut at least one branch from the Dramen tree. It's recommended you cut at least 4 branches so you don't have to return in future quests.");
 		teleportAway = new DetailedQuestStep(this, "Teleport away with the branches.", dramenBranch);
 		getAnotherBranch = new DetailedQuestStep(this, "If you've lost your Dramen branch/staff, you will need to return to Entrana and cut another. You will not need to defeat the Tree Spirit again.");

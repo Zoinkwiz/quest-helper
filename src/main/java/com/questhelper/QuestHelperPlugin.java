rheine15/runelite-quest-helper

Add AddNeededItems helper

src/main/java/com/questhelper/QuestHelperPlugin.java
@@ -46,6 +46,7 @@ import com.questhelper.questhelpers.BasicQuestHelper;
 import com.questhelper.questhelpers.ComplexStateQuestHelper;
 import com.questhelper.questhelpers.Quest;
 import com.questhelper.questhelpers.QuestHelper;
+import com.questhelper.requirements.Requirement;
 import com.questhelper.requirements.item.ItemRequirement;
 import com.questhelper.steps.QuestStep;
 import java.awt.image.BufferedImage;
@@ -57,6 +58,8 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.SortedMap;
+import java.util.TreeMap;
 import java.util.stream.Collectors;
 import javax.inject.Inject;
 import javax.inject.Named;
@@ -247,6 +250,8 @@ public class QuestHelperPlugin extends Plugin
 
 	private boolean displayNameKnown;
 
+	public SortedMap<String, List<ItemRequirement>> itemRequirements = new TreeMap<>();
+
 	@Getter
 	private int lastTickInventoryUpdated = -1;
 
@@ -348,6 +353,7 @@ public class QuestHelperPlugin extends Plugin
 		{
 			loadQuestList = false;
 			updateQuestList();
+			getAllItemRequirements();
 		}
 	}
 
@@ -722,6 +728,7 @@ public class QuestHelperPlugin extends Plugin
 			if (shouldUpdateList)
 			{
 				updateQuestList();
+				getAllItemRequirements();
 			}
 			if (bankTagsMain != null)
 			{
@@ -742,9 +749,21 @@ public class QuestHelperPlugin extends Plugin
 
 		scannedQuests.putAll(instantiate(classPath, QuestHelperPlugin.ACHIEVEMENT_PACKAGE));
 
+		return scannedQuests;
+	}
+
+	private void getAllItemRequirements()
+	{
+		List<QuestHelper> filteredQuests = quests.values()
+			.stream()
+			.filter(QuestHelperConfig.QuestFilter.QUEST)
+			.filter(Quest::isNotCompleted)
+			.sorted(config.orderListBy())
+			.collect(Collectors.toList());
+
 		clientThread.invokeLater(() -> {
-			List<ItemRequirement> itemRequirements = new ArrayList<>();
-			scannedQuests.forEach((String name, QuestHelper questHelper) -> {
+			SortedMap<String, List<ItemRequirement>> newReqs = new TreeMap<>();
+			filteredQuests.forEach((QuestHelper questHelper) -> {
 				eventBus.register(questHelper);
 				if (questHelper instanceof BasicQuestHelper)
 				{
@@ -755,20 +774,14 @@ public class QuestHelperPlugin extends Plugin
 				{
 					((ComplexStateQuestHelper) questHelper).loadStep();
 				}
-//				questHelper.setupRequirements();
 				if (questHelper.getItemRequirements() != null)
 				{
-					List<ItemRequirement> itemReqs = questHelper.getItemRequirements();
-					itemRequirements.addAll(questHelper.getItemRequirements());
+					newReqs.put(questHelper.getQuest().getName(), questHelper.getItemRequirements());
 				}
 				eventBus.unregister(questHelper);
 			});
-			for (ItemRequirement itemRequirement : itemRequirements)
-			{
-				System.out.println(itemRequirement.getName());
-			}
+			itemRequirements = newReqs;
 		});
-		return scannedQuests;
 	}
 
 	private Map<String, QuestHelper> instantiate(ClassPath classPath, String packageName)
@@ -838,6 +851,7 @@ public class QuestHelperPlugin extends Plugin
 			questHelper.setInjector(questInjector);
 			questHelper.setQuest(quest);
 			questHelper.setConfig(config);
+			questHelper.setQuestHelperPlugin(this);
 		}
 		catch (InstantiationException | IllegalAccessException | CreationException ex)
 		{

src/main/java/com/questhelper/QuestHelperQuest.java
@@ -348,6 +348,8 @@ public enum QuestHelperQuest
 
 
 	// Generic
+	CHECK_ITEMS("Check all items", QuestVarbits.QUEST_THE_MAGE_ARENA_II, -1, Quest.Type.GENERIC,
+		Quest.Difficulty.GENERIC),
 	MA2_LOCATOR("Mage Arena II Locator", QuestVarbits.QUEST_THE_MAGE_ARENA_II, -1, Quest.Type.GENERIC,
 		Quest.Difficulty.GENERIC),
 
@@ -422,6 +424,18 @@ public enum QuestHelperQuest
 		this.questType = questType;
 		this.difficulty = difficulty;
 		this.completeValue = completeValue;
+	}
+
+	QuestHelperQuest(String name, Quest.Type questType, Quest.Difficulty difficulty)
+	{
+		this.id = -1;
+		this.name = name;
+		this.keywords = titleToKeywords(name);
+		this.varbit = null;
+		this.varPlayer = null;
+		this.questType = questType;
+		this.difficulty = difficulty;
+		this.completeValue = -1;
 	}
 
 	// Can be used for skill based helpers

Zoinkwiz/quest-helper

src/main/java/com/questhelper/QuestHelperPlugin.java
@@ -57,6 +57,8 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.SortedMap;
+import java.util.TreeMap;
 import java.util.stream.Collectors;
 import javax.inject.Inject;
 import javax.inject.Named;
@@ -257,6 +259,8 @@ public class QuestHelperPlugin extends Plugin
 
 	private boolean displayNameKnown;
 
+	public SortedMap<String, List<ItemRequirement>> itemRequirements = new TreeMap<>();
+
 	@Getter
 	private Cheerer cheerer;
 
@@ -377,6 +381,7 @@ public class QuestHelperPlugin extends Plugin
 		{
 			loadQuestList = false;
 			updateQuestList();
+			getAllItemRequirements();
 		}
 	}
 
@@ -826,6 +831,7 @@ public class QuestHelperPlugin extends Plugin
 			if (shouldUpdateList)
 			{
 				updateQuestList();
+				getAllItemRequirements();
 			}
 			if (bankTagsMain != null)
 			{
@@ -846,9 +852,21 @@ public class QuestHelperPlugin extends Plugin
 
 		scannedQuests.putAll(instantiate(classPath, QuestHelperPlugin.ACHIEVEMENT_PACKAGE));
 
+		return scannedQuests;
+	}
+
+	private void getAllItemRequirements()
+	{
+		List<QuestHelper> filteredQuests = quests.values()
+			.stream()
+			.filter(QuestHelperConfig.QuestFilter.QUEST)
+			.filter(QuestDetails::isNotCompleted)
+			.sorted(config.orderListBy())
+			.collect(Collectors.toList());
+
 		clientThread.invokeLater(() -> {
-			List<ItemRequirement> itemRequirements = new ArrayList<>();
-			scannedQuests.forEach((String name, QuestHelper questHelper) -> {
+			SortedMap<String, List<ItemRequirement>> newReqs = new TreeMap<>();
+			filteredQuests.forEach((QuestHelper questHelper) -> {
 				eventBus.register(questHelper);
 				if (questHelper instanceof BasicQuestHelper)
 				{
@@ -859,20 +877,14 @@ public class QuestHelperPlugin extends Plugin
 				{
 					((ComplexStateQuestHelper) questHelper).loadStep();
 				}
-//				questHelper.setupRequirements();
 				if (questHelper.getItemRequirements() != null)
 				{
-					List<ItemRequirement> itemReqs = questHelper.getItemRequirements();
-					itemRequirements.addAll(questHelper.getItemRequirements());
+					newReqs.put(questHelper.getQuest().getName(), questHelper.getItemRequirements());
 				}
 				eventBus.unregister(questHelper);
 			});
-			for (ItemRequirement itemRequirement : itemRequirements)
-			{
-				System.out.println(itemRequirement.getName());
-			}
+			itemRequirements = newReqs;
 		});
-		return scannedQuests;
 	}
 
 	private Map<String, QuestHelper> instantiate(ClassPath classPath, String packageName)
@@ -942,6 +954,7 @@ public class QuestHelperPlugin extends Plugin
 			questHelper.setInjector(questInjector);
 			questHelper.setQuest(quest);
 			questHelper.setConfig(config);
+			questHelper.setQuestHelperPlugin(this);
 		}
 		catch (InstantiationException | IllegalAccessException | CreationException ex)
 		{

src/main/java/com/questhelper/QuestHelperQuest.java
@@ -350,6 +350,8 @@ public enum QuestHelperQuest
 
 
 	// Generic
+	CHECK_ITEMS("Check all items", QuestVarbits.QUEST_THE_MAGE_ARENA_II, -1, QuestDetails.Type.GENERIC,
+		QuestDetails.Difficulty.GENERIC),
 	MA2_LOCATOR("Mage Arena II Locator", QuestVarbits.QUEST_THE_MAGE_ARENA_II, -1, QuestDetails.Type.GENERIC,
 		QuestDetails.Difficulty.GENERIC),
 	DAG_ROUTE("Daganoth Kings Route", QuestVarbits.QUEST_THE_FREMENNIK_ISLES, -1, QuestDetails.Type.GENERIC,
@@ -454,6 +456,18 @@ public enum QuestHelperQuest
 		this.questType = questType;
 		this.difficulty = difficulty;
 		this.completeValue = completeValue;
+	}
+
+	QuestHelperQuest(String name, Quest.Type questType, Quest.Difficulty difficulty)
+	{
+		this.id = -1;
+		this.name = name;
+		this.keywords = titleToKeywords(name);
+		this.varbit = null;
+		this.varPlayer = null;
+		this.questType = questType;
+		this.difficulty = difficulty;
+		this.completeValue = -1;
 	}
 
 	// Can be used for skill based helpers
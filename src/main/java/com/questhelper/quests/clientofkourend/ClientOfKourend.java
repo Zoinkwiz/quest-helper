5478c70f5358a9fc36d00ab1eab21fbd159ed238
Bob Heine <rheine15@gmail.com>
2022-12-22 12:10:12 -0600

    Add games necklace to client of kourend recommended items

--- src/main/java/com/questhelper/quests/clientofkourend/ClientOfKourend.java
+++ src/main/java/com/questhelper/quests/clientofkourend/ClientOfKourend.java
@@ -56,6 +56,9 @@ public class ClientOfKourend extends BasicQuestHelper
 {
 	//Items Required
 	ItemRequirement feather;
+	
+	//Items Recommended
+	ItemRequirement gamesNecklace;
 
 	//Other items used
 	ItemRequirement enchantedScroll, enchantedQuill, mysteriousOrb;
@@ -105,6 +108,7 @@ public class ClientOfKourend extends BasicQuestHelper
 		feather.addAlternates(ItemID.BLUE_FEATHER, ItemID.ORANGE_FEATHER, ItemID.RED_FEATHER, ItemID.YELLOW_FEATHER,
 			ItemID.EAGLE_FEATHER, ItemID.STRIPY_FEATHER);
 		feather.setHighlightInInventory(true);
+		gamesNecklace = new ItemRequirement("Games necklace", ItemCollections.GAMES_NECKLACES);
 		enchantedScroll = new ItemRequirement("Enchanted scroll", ItemID.ENCHANTED_SCROLL);
 		enchantedScroll.setHighlightInInventory(true);
 		mysteriousOrb = new ItemRequirement("Mysterious orb", ItemID.MYSTERIOUS_ORB);
@@ -164,6 +168,12 @@ public class ClientOfKourend extends BasicQuestHelper
 		ArrayList<ItemRequirement> reqs = new ArrayList<>();
 		reqs.add(feather);
 		return reqs;
+	}
+	
+	@Override
+	public List<ItemRequirement> getItemRecommended()
+	{
+		return Arrays.asList(gamesNecklace)
 	}
 	
 	@Override

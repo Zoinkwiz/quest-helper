8aa0f3cf676edf083c59b59f2a381e77811eb913
Zoinkwiz <zoinkwiz@hotmail.co.uk>
2023-05-31 13:10:55 +0100

    Fix checks on herb state

--- src/main/java/com/questhelper/helpers/mischelpers/herbrun/FarmingHandler.java
+++ src/main/java/com/questhelper/helpers/mischelpers/herbrun/FarmingHandler.java
@@ -81,6 +81,9 @@ public class FarmingHandler
 
 		PatchState state = patch.getImplementation().forVarbitValue(value);
 		if (state == null) return null;
+		if (state.getCropState() == CropState.EMPTY) return CropState.EMPTY;
+		if (state.getProduce() == Produce.WEEDS) return CropState.EMPTY;
+		if (state.getCropState() == CropState.DEAD) return CropState.DEAD;
 
 		if (unixTime <= 0)
 		{
@@ -91,16 +94,7 @@ public class FarmingHandler
 		int stages = state.getStages();
 		int tickrate = state.getTickRate();
 
-		boolean autoweed = client.getVarbitValue(Varbits.AUTOWEED) == 2;
-
 		boolean botanist = client.getVarbitValue(Varbits.LEAGUE_RELIC_5) == 1;
-
-		if (autoweed && state.getProduce() == Produce.WEEDS)
-		{
-			stage = 0;
-			stages = 1;
-			tickrate = 0;
-		}
 
 		if (botanist)
 		{
@@ -114,8 +108,6 @@ public class FarmingHandler
 			doneEstimate = getTickTime(tickrate, stages - 1 - stage, tickTime, profile);
 		}
 
-		if (stage == 0) return CropState.EMPTY;
-		if (state.getCropState() == CropState.DEAD) return CropState.DEAD;
 		if (unixNow >= doneEstimate) return CropState.HARVESTABLE;
 
 		return CropState.GROWING;

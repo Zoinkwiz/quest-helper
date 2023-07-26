e3cf055370891a693e34d3008c54d0a497bcd7c0
Julia Lerebäck Corell <112405931+jLereback@users.noreply.github.com>
2023-07-26 13:00:20 +0200

    Refactor Agility and add Mining (#1192)

--- src/main/java/com/questhelper/helpers/skills/agility/Agility.java
+++ src/main/java/com/questhelper/helpers/skills/agility/Agility.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022, jLereback <https://github.com/jLereback>
+ * Copyright (c) 2023, jLereback <https://github.com/jLereback, JesperBodin <https://github.com/JesperBodin>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -33,78 +33,106 @@ import com.questhelper.requirements.conditional.Conditions;
 import com.questhelper.requirements.item.ItemRequirement;
 import com.questhelper.requirements.item.ItemRequirements;
 import com.questhelper.requirements.player.SkillRequirement;
+import com.questhelper.requirements.quest.QuestRequirement;
 import com.questhelper.requirements.util.LogicType;
 import com.questhelper.rewards.UnlockReward;
 import com.questhelper.steps.ConditionalStep;
-import com.questhelper.steps.ObjectStep;
 import com.questhelper.steps.QuestStep;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import net.runelite.api.ItemID;
-import net.runelite.api.ObjectID;
-import net.runelite.api.Skill;
-import net.runelite.api.coords.WorldPoint;
+import net.runelite.api.QuestState;
+import static net.runelite.api.Skill.AGILITY;
 
 @QuestDescriptor(
 	quest = QuestHelperQuest.AGILITY
 )
 public class Agility extends ComplexStateQuestHelper
 {
-
 	// Items recommended
-	ItemRequirement bootsOfLightness, gracefulOutfit, gracefulHood, gracefulTop, gracefulLegs, gracefulGloves, gracefulBoots, gracefulCape;
+	ItemRequirement bootsOfLightness, gracefulOutfit, gracefulHood, gracefulTop,
+		gracefulLegs, gracefulGloves, gracefulBoots, gracefulCape;
 
 	SkillRequirement ag10, ag20, ag30, ag40, ag50, ag60, ag70, ag80, ag90;
 
 	SkillRequirement ag45;
 
-	QuestStep gnomeStronghold, draynorVillage, alKharid, varrock, canifis, falador, seersVillage, pollnivneach, rellekka, ardougne;
+	AgilityCourse gnomeStronghold, draynorVillage, alKharid, varrock, canifis,
+		falador, seersVillage, pollnivneach, rellekka, ardougne;
+	ConditionalStep gnomeStep, draynorStep, alKharidStep, varrockStep, canifisStep,
+		faladorStep, seersStep, pollnivneachStep, rellekkaStep, ardougneStep;
 
 	@Override
 	public QuestStep loadStep()
 	{
 		setupRequirements();
-		setupSteps();
 
-		ConditionalStep fullTraining = new ConditionalStep(this, gnomeStronghold);
-		fullTraining.addStep(ag90, ardougne, true);
-		fullTraining.addStep(ag80, rellekka, true);
-		fullTraining.addStep(ag70, pollnivneach, true);
-		fullTraining.addStep(ag60, seersVillage, true);
-		fullTraining.addStep(ag50, falador, true);
-		fullTraining.addStep(ag40, canifis, true);
-		fullTraining.addStep(ag30, varrock, true);
-		fullTraining.addStep(ag20, alKharid, true);
-		fullTraining.addStep(ag10, draynorVillage, true);
+		gnomeStep = gnomeStronghold.loadStep();
+		draynorStep = draynorVillage.loadStep();
+		alKharidStep = alKharid.loadStep();
+		varrockStep = varrock.loadStep();
+		canifisStep = canifis.loadStep();
+		faladorStep = falador.loadStep();
+		seersStep = seersVillage.loadStep();
+		pollnivneachStep = pollnivneach.loadStep();
+		rellekkaStep = rellekka.loadStep();
+		ardougneStep = ardougne.loadStep();
 
-		return fullTraining;
+
+		ConditionalStep superStep = new ConditionalStep(this, gnomeStep);
+		superStep.addStep(ag90, ardougneStep);
+		superStep.addStep(ag80, rellekkaStep);
+		superStep.addStep(new Conditions(LogicType.NAND, ag70,
+			new QuestRequirement(QuestHelperQuest.KANDARIN_HARD, QuestState.FINISHED)), pollnivneachStep);
+		superStep.addStep(ag60, seersStep);
+		superStep.addStep(ag40, canifisStep);
+		superStep.addStep(ag50, faladorStep);
+		superStep.addStep(ag30, varrockStep);
+		superStep.addStep(ag20, alKharidStep);
+		superStep.addStep(ag10, draynorStep);
+
+		return superStep;
 	}
 
 	@Override
 	public void setupRequirements()
 	{
-		ag10 = new SkillRequirement(Skill.AGILITY, 10);
-		ag20 = new SkillRequirement(Skill.AGILITY, 20);
-		ag30 = new SkillRequirement(Skill.AGILITY, 30);
-		ag40 = new SkillRequirement(Skill.AGILITY, 40);
-		ag50 = new SkillRequirement(Skill.AGILITY, 50);
-		ag60 = new SkillRequirement(Skill.AGILITY, 60);
-		ag70 = new SkillRequirement(Skill.AGILITY, 70);
-		ag80 = new SkillRequirement(Skill.AGILITY, 80);
-		ag90 = new SkillRequirement(Skill.AGILITY, 90);
+		//Setup courses
+		gnomeStronghold = new GnomeStronghold(this);
+		draynorVillage = new DraynorVillage(this);
+		alKharid = new AlKharid(this);
+		varrock = new Varrock(this);
+		canifis = new Canifis(this);
+		falador = new Falador(this);
+		seersVillage = new SeersVillage(this);
+		pollnivneach = new Pollnivneach(this);
+		rellekka = new Rellekka(this);
+		ardougne = new Ardougne(this);
 
-		ag45 = new SkillRequirement(Skill.AGILITY, 45);
+		//Setup skill requirements
+		ag10 = new SkillRequirement(AGILITY, 10);
+		ag20 = new SkillRequirement(AGILITY, 20);
+		ag30 = new SkillRequirement(AGILITY, 30);
+		ag40 = new SkillRequirement(AGILITY, 40);
+		ag50 = new SkillRequirement(AGILITY, 50);
+		ag60 = new SkillRequirement(AGILITY, 60);
+		ag70 = new SkillRequirement(AGILITY, 70);
+		ag80 = new SkillRequirement(AGILITY, 80);
+		ag90 = new SkillRequirement(AGILITY, 90);
+
+		ag45 = new SkillRequirement(AGILITY, 45);
 
 
+		//Setup item requirements
 		bootsOfLightness = new ItemRequirement(
 			"Boots of Lightness", ItemID.BOOTS_OF_LIGHTNESS).showConditioned(
 			new Conditions(LogicType.NOR, ag45)
 		).isNotConsumed();
 
 		gracefulHood = new ItemRequirement(
-			"Graceful hood", ItemCollections.GRACEFUL_HOOD, 1 ,true).showConditioned(
+			"Graceful hood", ItemCollections.GRACEFUL_HOOD, 1, true).showConditioned(
 			new Conditions(ag45)
 		).isNotConsumed();
 
@@ -138,48 +166,25 @@ public class Agility extends ComplexStateQuestHelper
 			"Graceful outfit (equipped)",
 			gracefulHood, gracefulTop, gracefulLegs, gracefulGloves, gracefulBoots, gracefulCape
 		).isNotConsumed();
-	}
 
-	private void setupSteps()
-	{
-		gnomeStronghold = new ObjectStep(this, ObjectID.LOG_BALANCE_23145, new WorldPoint(2474, 3435, 0),
-			"Train agility at the Gnome Stronghold Agility Course.",  Collections.EMPTY_LIST, Collections.singletonList(gracefulOutfit));
 
-		draynorVillage = new ObjectStep(this, ObjectID.ROUGH_WALL, new WorldPoint(3103, 3279, 0),
-			"Train agility at the Draynor Village Rooftop Course.",  Collections.EMPTY_LIST, Collections.singletonList(gracefulOutfit));
-
-		alKharid = new ObjectStep(this, ObjectID.ROUGH_WALL_11633, new WorldPoint(3273, 3195, 0),
-			"Train agility at the Al Kharid Rooftop Course.",  Collections.EMPTY_LIST, Collections.singletonList(gracefulOutfit));
-
-		varrock = new ObjectStep(this, ObjectID.ROUGH_WALL_14412, new WorldPoint(3221, 3414, 0),
-			"Train agility at the Varrock Rooftop Course.", Collections.EMPTY_LIST, Collections.singletonList(gracefulOutfit));
-
-		canifis = new ObjectStep(this, ObjectID.TALL_TREE_14843, new WorldPoint(3508, 3488, 0),
-			"Train agility at the Canifis Rooftop Course. For best spawn of Mark of Grace, " +
-				"stay on this course until 60 Agility.", Collections.EMPTY_LIST, Collections.singletonList(gracefulOutfit));
-
-		falador = new ObjectStep(this, ObjectID.ROUGH_WALL_14898, new WorldPoint(3036, 3341, 0),
-			"Train agility at the Falador Rooftop Course. For best spawn of Mark of Grace," +
-				" stay on Canifis Rooftop Course until 60 Agility, then go directly to Seer's Village.", Collections.EMPTY_LIST, Collections.singletonList(gracefulOutfit));
-
-		seersVillage = new ObjectStep(this, ObjectID.WALL_14927, new WorldPoint(2729, 3489, 0),
-			"Train agility at Seer's Village Rooftop Course.", Collections.EMPTY_LIST, Collections.singletonList(gracefulOutfit));
-
-		pollnivneach = new ObjectStep(this, ObjectID.BASKET_14935, new WorldPoint(3351, 2962, 0),
-			"Train agility at the Pollnivneach Rooftop Course.", Collections.EMPTY_LIST, Collections.singletonList(gracefulOutfit));
-
-		rellekka = new ObjectStep(this, ObjectID.ROUGH_WALL_14946, new WorldPoint(2625, 2677, 0),
-			"Train agility at the Rellekka Rooftop Course.", Collections.EMPTY_LIST, Collections.singletonList(gracefulOutfit));
-
-		ardougne = new ObjectStep(this, ObjectID.WOODEN_BEAMS, new WorldPoint(2673, 3298, 0),
-			"Train agility at the Ardougne Rooftop Course.", Collections.EMPTY_LIST, Collections.singletonList(gracefulOutfit));
+		gnomeStronghold.setRecommended(bootsOfLightness);
+		draynorVillage.setRecommended(bootsOfLightness);
+		alKharid.setRecommended(bootsOfLightness);
+		varrock.setRecommended(bootsOfLightness);
+		canifis.setRecommended(bootsOfLightness, gracefulOutfit);
+		falador.setRecommended(gracefulOutfit);
+		seersVillage.setRecommended(gracefulOutfit);
+		pollnivneach.setRecommended(gracefulOutfit);
+		rellekka.setRecommended(gracefulOutfit);
+		ardougne.setRecommended(gracefulOutfit);
 	}
 
 	@Override
 	public List<UnlockReward> getUnlockRewards()
 	{
 		return Collections.singletonList(
-			new UnlockReward("Ability to purchase AGILITY Cape for 99k")
+			new UnlockReward("Ability to purchase agility Cape for 99k")
 		);
 	}
 
@@ -195,36 +200,29 @@ public class Agility extends ComplexStateQuestHelper
 	public List<PanelDetails> getPanels()
 	{
 		List<PanelDetails> allSteps = new ArrayList<>();
-		allSteps.add(
-			new PanelDetails("1 - 10: Gnome Stronghold", Collections.singletonList(gnomeStronghold))
-		);
-		allSteps.add(
-			new PanelDetails("10 - 20: Draynor Village", Collections.singletonList(draynorVillage))
-		);
-		allSteps.add(
-			new PanelDetails("20 - 30: Al Kharid", Collections.singletonList(alKharid))
-		);
-		allSteps.add(
-			new PanelDetails("30 - 40: Varrock", Collections.singletonList(varrock))
-		);
-		allSteps.add(
-			new PanelDetails("40 - 50/60: Canifis", Collections.singletonList(canifis))
-		);
-		allSteps.add(
-			new PanelDetails("50 - 60: Falador", Collections.singletonList(falador))
-		);
-		allSteps.add(
-			new PanelDetails("60 - 70: Seer's Village", Collections.singletonList(seersVillage))
-		);
-		allSteps.add(
-			new PanelDetails("70 - 80: Pollnivneach", Collections.singletonList(pollnivneach))
-		);
-		allSteps.add(
-			new PanelDetails("80 - 90: Rellekka", Collections.singletonList(rellekka))
-		);
-		allSteps.add(
-			new PanelDetails("90 - 99: Ardougne", Collections.singletonList(ardougne))
-		);
+
+		allSteps.add(gnomeStronghold.getPanelDetails());
+		allSteps.add(draynorVillage.getPanelDetails());
+		allSteps.add(alKharid.getPanelDetails());
+		allSteps.add(varrock.getPanelDetails());
+		allSteps.add(canifis.getPanelDetails());
+		allSteps.add(falador.getPanelDetails());
+		allSteps.add(seersVillage.getPanelDetails());
+		allSteps.add(pollnivneach.getPanelDetails());
+		allSteps.add(rellekka.getPanelDetails());
+		allSteps.add(ardougne.getPanelDetails());
+
+
 		return allSteps;
+	}
+
+	@Override
+	public List<String> getNotes()
+	{
+		return Arrays.asList("40-60 Agility: Stay on Canifis Rooftop Course for best spawn of Mark of Grace" +
+				" until 60 Agility, then go directly to Seer's Village\n\n",
+			"60-80 Agility: If completed Kandarin Hard Diary, configure the Camelot Teleport Spell" +
+				" to Seer's and stay on Seer's rooftop course until 80 Agility." +
+				" After each completed lap, use the teleport spell to get close to the course starting point");
 	}
 }

3fd7385e86839cd06d2f4370b6d24149318d016b
Zoinkwiz <zoinkwiz@hotmail.co.uk>
2023-08-26 21:22:32 +0100

    Use DialogRequirement for Lady of the Lake check

--- src/main/java/com/questhelper/helpers/quests/merlinscrystal/MerlinsCrystal.java
+++ src/main/java/com/questhelper/helpers/quests/merlinscrystal/MerlinsCrystal.java
@@ -36,6 +36,7 @@ import com.questhelper.requirements.ZoneRequirement;
 import com.questhelper.requirements.conditional.Conditions;
 import com.questhelper.requirements.conditional.NpcCondition;
 import com.questhelper.requirements.conditional.ObjectCondition;
+import com.questhelper.requirements.npc.DialogRequirement;
 import com.questhelper.requirements.widget.WidgetTextRequirement;
 import com.questhelper.requirements.util.LogicType;
 import com.questhelper.rewards.ItemReward;
@@ -200,7 +201,7 @@ public class MerlinsCrystal extends BasicQuestHelper
 		clearedHive = new ObjectCondition(ObjectID.BEEHIVE_305);
 		hasAnyBlackCandle = new Conditions(LogicType.OR, blackCandle, litBlackCandle);
 		beggarNearby = new NpcCondition(NpcID.BEGGAR);
-		talkedToLady = new WidgetTextRequirement(217, 6, "Ok. That seems easy enough.");
+		talkedToLady = new Conditions(true, new DialogRequirement(client.getLocalPlayer().getName(), "Ok. That seems easy enough."));
 		hasReadSpell = new Conditions(true, LogicType.AND, new WidgetTextRequirement(229, 1, "You find a small inscription"));
 		inStar = new ZoneRequirement(star);
 		thrantaxNearby = new NpcCondition(NpcID.THRANTAX_THE_MIGHTY);

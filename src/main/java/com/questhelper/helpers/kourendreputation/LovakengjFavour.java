69d1d1ffde327abd5cdc1defdd2606d1b66fef98
Zoinkwiz <zoinkwiz@hotmail.co.uk>
2023-09-01 11:11:36 +0100

    Update AA last step detection

---
+++ src/main/java/com/questhelper/helpers/kourendreputation/LovakengjFavour.java
@@ -0,0 +1,23 @@
+package com.questhelper.helpers.kourendreputation;
+
+public class LovakengjFavour
+{
+	// Miriam
+	// 'Can I ride your minecart?'
+	// 'Alright, what do you need me to do?'
+	// 5085 0->1
+	// 'Have you got any more scrolls?'
+
+	// Arceuus, Loinur
+	// No indicator of done??
+	// Loinur|Thanks, human. I guess it's okay for you to ride my carts after all. So, can I help you?
+
+	// Pisc
+	// Raeli |Thanks, human. I guess it's okay for you to ride my carts after all. So, can I help you?
+
+	// Mogrim|Thanks, human. I guess it's okay for you to ride my carts after all. So, can I help you?
+
+	// Stuliette|Thanks, human. I guess it's okay for you to ride my carts after all. So, can I help you?
+
+	// Traxi|Thanks, human. I guess it's okay for you to ride my carts after all. So, can I help you?
+}

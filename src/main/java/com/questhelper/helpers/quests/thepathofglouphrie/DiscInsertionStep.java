f1c4f43c608c3c6abe7f9f40de885277834a525a
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2023-10-01 18:20:48 +0200

    DiscInsertionStep: Add comments

--- src/main/java/com/questhelper/helpers/quests/thepathofglouphrie/DiscInsertionStep.java
+++ src/main/java/com/questhelper/helpers/quests/thepathofglouphrie/DiscInsertionStep.java
@@ -6,6 +6,9 @@ import com.questhelper.steps.ItemStep;
 import net.runelite.api.coords.WorldPoint;
 import net.runelite.api.widgets.Widget;
 
+/**
+ * DiscInsertionStep is a custom ItemStep that allows highlighting items in the disc insertion widget that overlays the inventory
+ */
 public class DiscInsertionStep extends ItemStep
 {
 	public DiscInsertionStep(QuestHelper questHelper, String text, Requirement... requirements)

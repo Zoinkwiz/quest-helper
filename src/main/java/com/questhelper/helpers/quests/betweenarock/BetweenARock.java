f51e9fe520f8a313dc4edc6248ca1cfc5b0fa899
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2024-01-11 10:14:10 +0100

    Between A Rock: Fix Ammo Mould quantity/Double Ammo Mould alternative

--- src/main/java/com/questhelper/helpers/quests/betweenarock/BetweenARock.java
+++ src/main/java/com/questhelper/helpers/quests/betweenarock/BetweenARock.java
@@ -202,7 +202,8 @@ public class BetweenARock extends BasicQuestHelper
 		goldCannonballHighlight = new ItemRequirement("Cannon ball", ItemID.CANNON_BALL);
 		goldCannonballHighlight.setHighlightInInventory(true);
 
-		cannonMould = new ItemRequirement("Ammo mould", ItemID.AMMO_MOULD, ItemID.DOUBLE_AMMO_MOULD);
+		cannonMould = new ItemRequirement("Ammo mould", ItemID.AMMO_MOULD, 1);
+		cannonMould.addAlternates(ItemID.DOUBLE_AMMO_MOULD);
 		cannonMould.setTooltip("You can buy one from Nulodion above the Dwarven Mine for 5 coins");
 
 		schematic = new ItemRequirement("Schematic", ItemID.SCHEMATIC);

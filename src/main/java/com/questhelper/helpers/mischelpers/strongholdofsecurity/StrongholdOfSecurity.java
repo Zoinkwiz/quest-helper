c2c42b95534dbd771014cc71d691f0201cdcf4f0
Zoinkwiz <zoinkwiz@hotmail.co.uk>
2024-02-25 16:20:37 +0000

    Apply suggestions from code review

--- src/main/java/com/questhelper/helpers/mischelpers/strongholdofsecurity/StrongholdOfSecurity.java
+++ src/main/java/com/questhelper/helpers/mischelpers/strongholdofsecurity/StrongholdOfSecurity.java
@@ -82,7 +82,7 @@ public class StrongholdOfSecurity extends BasicQuestHelper
 		"Delete it - it's a fake!",
 		"Don't give them my password.",
 		"Report the player for phishing.",
-		"Use the Account Recovery System.",
+		"Use the Account Recovery system.",
 		"No way! I'm reporting you to Jagex!",
 		"No, you should never buy an account.",
 		"Secure my device and reset my password.",
@@ -93,7 +93,7 @@ public class StrongholdOfSecurity extends BasicQuestHelper
 		"Virus scan my device then change my password.",
 		"Report the incident and do not click any links.",
 		"Don't share your information and report the player.",
-		"Set up 2 step authentication with my email provider.",
+		"Set up two-factor authentication with my email provider.",
 		"No, you should never allow anyone to level your account.",
 		"Authenticator and two-step login on my registered email.",
 		"No way! You'll just take my gold for your own! Reported!",
@@ -103,7 +103,9 @@ public class StrongholdOfSecurity extends BasicQuestHelper
 		"Politely tell them no and then use the 'Report Abuse' button.",
 		"Don't give out your password to anyone. Not even close friends.",
 		"Do not visit the website and report the player who messaged you.",
-		"Report the stream as a scam. Real Jagex streams have a 'verified' mark."
+		"Report the stream as a scam. Real Jagex streams have a 'verified' mark.",
+		"Two-factor authentication on yuor account and your registered email.",
+		"Nope, you're tricking me into going somewhere dangerous."
 	};
 
 	int[] cbLevels = {26, 51, 76};

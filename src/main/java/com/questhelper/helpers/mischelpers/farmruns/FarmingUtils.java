9b7a721b476b1b7c09a41b63f8c55be02f597652
Zoinkwiz <zoinkwiz@hotmail.co.uk>
2024-06-06 16:05:10 +0100

    Cut down on amount of manual setup in TreeRun

--- src/main/java/com/questhelper/helpers/mischelpers/farmruns/FarmingUtils.java
+++ src/main/java/com/questhelper/helpers/mischelpers/farmruns/FarmingUtils.java
@@ -1,10 +1,13 @@
 package com.questhelper.helpers.mischelpers.farmruns;
 
+import com.questhelper.QuestHelperConfig;
 import com.questhelper.requirements.item.ItemRequirement;
+import java.util.ArrayList;
+import java.util.List;
 import net.runelite.api.Client;
 import net.runelite.api.ItemID;
-import net.runelite.api.Varbits;
 import net.runelite.client.config.ConfigManager;
+import net.runelite.client.game.ItemManager;
 import net.runelite.client.util.Text;
 
 public class FarmingUtils
@@ -24,7 +27,7 @@ public class FarmingUtils
 	 */
 
 	public static ItemRequirement createSeedRequirement(ConfigManager configManager, String configGroupName,
-			String seedConfigName, Enum<?> defaultValue, int defaultItemId)
+														String seedConfigName, Enum<?> defaultValue, int defaultItemId)
 	{
 
 		ItemRequirement seedRequirement = new ItemRequirement(Text.titleCase(defaultValue), defaultItemId);
@@ -55,7 +58,7 @@ public class FarmingUtils
 			}
 		}
 		else
-			{
+		{
 			configManager.setConfiguration(configGroupName, seedConfigName, defaultValue.name());
 		}
 
@@ -95,43 +98,186 @@ public class FarmingUtils
 		return false;
 	}
 
-	public enum TreeSapling
+	public static String getEnumFromConfig(ConfigManager configManager, ConfigEnum configEnum)
 	{
-		OAK(ItemID.OAK_SAPLING), WILLOW(ItemID.WILLOW_SAPLING), MAPLE(ItemID.MAPLE_SAPLING), YEW(ItemID.YEW_SAPLING),
-		MAGIC(ItemID.MAGIC_SAPLING);
+		try
+		{
+			return configManager.getRSProfileConfiguration(QuestHelperConfig.QUEST_BACKGROUND_GROUP, configEnum.getConfigKey());
+		}
+		catch (Exception err)
+		{
+			configManager.setRSProfileConfiguration(QuestHelperConfig.QUEST_BACKGROUND_GROUP, configEnum.getDefault().getConfigKey(), configEnum.getDefault());
+			return null;
+		}
+	}
+
+	public interface ConfigEnum
+	{
+		String getConfigKey();
+
+		ConfigEnum getDefault();
+	}
+
+	public interface PlantableItem extends ConfigEnum
+	{
+		int getPlantableItemId();
+
+		int getProtectionItemId();
+
+		int getProtectionItemQuantity();
+
+		default ItemRequirement getPlantableItemRequirement(ItemManager itemManager)
+		{
+			return new ItemRequirement(itemManager.getItemComposition(getPlantableItemId()).getName(), getPlantableItemId());
+		}
+
+		default ItemRequirement getProtectionItemRequirement(ItemManager itemManager)
+		{
+			return new ItemRequirement(itemManager.getItemComposition(getProtectionItemId()).getName(), getProtectionItemId(), getProtectionItemQuantity());
+		}
+	}
+
+	public enum TreeSapling implements PlantableItem
+	{
+		OAK(ItemID.OAK_SAPLING, ItemID.TOMATOES5, 1), WILLOW(ItemID.WILLOW_SAPLING, ItemID.APPLES5, 1),
+		MAPLE(ItemID.MAPLE_SAPLING, ItemID.ORANGES5, 1), YEW(ItemID.YEW_SAPLING, ItemID.CACTUS_SPINE, 10),
+		MAGIC(ItemID.MAGIC_SAPLING, ItemID.COCONUT, 25);
 
 		final int treeSaplingID;
+		final int protectionItemId;
+		final int protectionItemQuantity;
 
-		TreeSapling(int treeSaplingID)
+		TreeSapling(int treeSaplingID, int protectionItemId, int protectionItemQuantity)
 		{
 			this.treeSaplingID = treeSaplingID;
+			this.protectionItemId = protectionItemId;
+			this.protectionItemQuantity = protectionItemQuantity;
+		}
+
+		@Override
+		public String getConfigKey()
+		{
+			return "treeSaplings";
+		}
+
+		@Override
+		public int getPlantableItemId()
+		{
+			return treeSaplingID;
+		}
+
+		@Override
+		public int getProtectionItemId()
+		{
+			return protectionItemId;
+		}
+
+		@Override
+		public int getProtectionItemQuantity()
+		{
+			return protectionItemQuantity;
+		}
+
+		@Override
+		public ConfigEnum getDefault()
+		{
+			return TreeSapling.OAK;
 		}
 	}
 
-	public enum FruitTreeSapling
+	public enum FruitTreeSapling implements PlantableItem
 	{
-		APPLE(ItemID.APPLE_SAPLING), BANANA(ItemID.BANANA_SAPLING), ORANGE(ItemID.ORANGE_SAPLING),
-		CURRY(ItemID.CURRY_SAPLING), PINEAPPLE(ItemID.PINEAPPLE_SAPLING), PAPAYA(ItemID.PAPAYA_SAPLING),
-		PALM(ItemID.PALM_SAPLING), DRAGONFRUIT(ItemID.DRAGONFRUIT_SAPLING);
+		APPLE(ItemID.APPLE_SAPLING, ItemID.SWEETCORN, 9), BANANA(ItemID.BANANA_SAPLING, ItemID.APPLES5, 4),
+		ORANGE(ItemID.ORANGE_SAPLING, ItemID.STRAWBERRIES5, 3), CURRY(ItemID.CURRY_SAPLING, ItemID.BANANAS5, 5),
+		PINEAPPLE(ItemID.PINEAPPLE_SAPLING, ItemID.WATERMELON, 10), PAPAYA(ItemID.PAPAYA_SAPLING, ItemID.PINEAPPLE, 10),
+		PALM(ItemID.PALM_SAPLING, ItemID.PAPAYA_FRUIT, 15), DRAGONFRUIT(ItemID.DRAGONFRUIT_SAPLING, ItemID.COCONUT, 15);
 
 		final int fruitTreeSaplingId;
+		final int protectionItemId;
+		final int protectionItemQuantity;
 
-		FruitTreeSapling(int fruitTreeSaplingId)
+		FruitTreeSapling(int fruitTreeSaplingId, int protectionItemId, int protectionItemQuantity)
 		{
 			this.fruitTreeSaplingId = fruitTreeSaplingId;
+			this.protectionItemId = protectionItemId;
+			this.protectionItemQuantity = protectionItemQuantity;
+		}
+
+		@Override
+		public String getConfigKey()
+		{
+			return "fruitTreeSaplings";
+		}
+
+		@Override
+		public int getPlantableItemId()
+		{
+			return fruitTreeSaplingId;
+		}
+
+		@Override
+		public int getProtectionItemId()
+		{
+			return protectionItemId;
+		}
+
+		@Override
+		public int getProtectionItemQuantity()
+		{
+			return protectionItemQuantity;
+		}
+
+		@Override
+		public ConfigEnum getDefault()
+		{
+			return FruitTreeSapling.APPLE;
 		}
 	}
 
-	public enum HardwoodTreeSapling
+	public enum HardwoodTreeSapling implements PlantableItem
 	{
-		TEAK(ItemID.TEAK_SAPLING),
-		MAHOGANY(ItemID.MAHOGANY_SAPLING);
+		TEAK(ItemID.TEAK_SAPLING, ItemID.LIMPWURT_ROOT, 15),
+		MAHOGANY(ItemID.MAHOGANY_SAPLING, ItemID.YANILLIAN_HOPS, 25);
 
 		final int hardwoodTreeSaplingId;
+		final int protectionItemId;
+		final int protectionItemQuantity;
 
-		HardwoodTreeSapling(int hardwoodTreeSaplingId)
+		HardwoodTreeSapling(int hardwoodTreeSaplingId, int protectionItemId, int protectionItemQuantity)
 		{
 			this.hardwoodTreeSaplingId = hardwoodTreeSaplingId;
+			this.protectionItemId = protectionItemId;
+			this.protectionItemQuantity = protectionItemQuantity;
+		}
+
+		@Override
+		public String getConfigKey()
+		{
+			return "hardwoodTreeSaplings";
+		}
+
+		@Override
+		public int getPlantableItemId()
+		{
+			return hardwoodTreeSaplingId;
+		}
+
+		@Override
+		public int getProtectionItemId()
+		{
+			return protectionItemId;
+		}
+
+		@Override
+		public int getProtectionItemQuantity()
+		{
+			return protectionItemQuantity;
+		}
+
+		@Override
+		public ConfigEnum getDefault()
+		{
+			return HardwoodTreeSapling.TEAK;
 		}
 	}
 
@@ -140,6 +286,19 @@ public class FarmingUtils
 		NONE(),
 		GRACEFUL(),
 		FARMING();
+	}
+
+	public enum PayOrCut
+	{
+		PAY(),
+		DIG();
+	}
+
+	public enum PayOrCompost
+	{
+		PAY(),
+		COMPOST(),
+		NEITHER();
 	}
 }
 

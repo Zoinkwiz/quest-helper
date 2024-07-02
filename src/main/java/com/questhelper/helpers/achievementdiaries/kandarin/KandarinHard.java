0263694dcb17dfacd4392481e917485957cd2d10
Zoinkwiz <zoinkwiz@hotmail.co.uk>
2024-07-02 21:01:30 +0100

    fix: Adjust Kandarin Hard/Elite potential skipped initialization
    This is due to a report from nightfirecat on the Runelite Discord, of the condition for the quest requirements not being initialized in a certain scenario.
    
    I wasn't able to reproduce it, but this should ensure that it shouldn't be possible at the very least.

--- src/main/java/com/questhelper/helpers/achievementdiaries/kandarin/KandarinHard.java
+++ src/main/java/com/questhelper/helpers/achievementdiaries/kandarin/KandarinHard.java
@@ -305,7 +305,7 @@ public class KandarinHard extends ComplexStateQuestHelper
 	@Override
 	public List<Requirement> getGeneralRequirements()
 	{
-		setupGeneralRequirements();
+		initializeRequirements();
 		ArrayList<Requirement> req = new ArrayList<>();
 
 		req.add(new SkillRequirement(Skill.AGILITY, 60, true));

3d2389062230ff68b45e8198d4aff81d47c946d4
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2024-12-14 13:45:16 +0100

    modernize

--- src/main/java/com/questhelper/helpers/quests/misthalinmystery/MisthalinMystery.java
+++ src/main/java/com/questhelper/helpers/quests/misthalinmystery/MisthalinMystery.java
@@ -78,49 +78,50 @@ public class MisthalinMystery extends BasicQuestHelper
 		initializeRequirements();
 		setupConditions();
 		setupSteps();
-		Map<Integer, QuestStep> steps = new HashMap<>();
+
+		var steps = new HashMap<Integer, QuestStep>();
 
 		steps.put(0, talkToAbigale);
 
 		steps.put(5, talkToAbigale);
 
-		ConditionalStep investigatingTheBarrel = new ConditionalStep(this, takeTheBoat);
+		var investigatingTheBarrel = new ConditionalStep(this, takeTheBoat);
 		investigatingTheBarrel.addStep(new Conditions(onIsland, bucket), searchTheBarrel);
 		investigatingTheBarrel.addStep(onIsland, takeTheBucket);
 		steps.put(10, investigatingTheBarrel);
 		steps.put(15, investigatingTheBarrel);
 
-		ConditionalStep emptyTheBarrel = new ConditionalStep(this, takeTheBoat);
+		var emptyTheBarrel = new ConditionalStep(this, takeTheBoat);
 		emptyTheBarrel.addStep(new Conditions(onIsland, bucket), useBucketOnBarrel);
 		emptyTheBarrel.addStep(onIsland, takeTheBucket);
 		steps.put(20, emptyTheBarrel);
 
-		ConditionalStep enterTheHouse = new ConditionalStep(this, takeTheBoat);
+		var enterTheHouse = new ConditionalStep(this, takeTheBoat);
 		enterTheHouse.addStep(new Conditions(onIsland, manorKey), openManorDoor);
 		enterTheHouse.addStep(onIsland, searchTheBarrelForKey);
 		steps.put(25, enterTheHouse);
 
-		ConditionalStep pinkDoor = new ConditionalStep(this, takeTheBoat);
+		var pinkDoor = new ConditionalStep(this, takeTheBoat);
 		pinkDoor.addStep(knife, tryToOpenPinkKnobDoor);
 		pinkDoor.addStep(onIsland, takeKnife);
 		steps.put(30, pinkDoor);
 
-		ConditionalStep pickUpAndReadNotes1 = new ConditionalStep(this, takeTheBoat);
+		var pickUpAndReadNotes1 = new ConditionalStep(this, takeTheBoat);
 		pickUpAndReadNotes1.addStep(new Conditions(onIsland, notes1), readNotes1);
 		pickUpAndReadNotes1.addStep(onIsland, takeNote1);
 		steps.put(35, pickUpAndReadNotes1);
 
-		ConditionalStep cutPainting = new ConditionalStep(this, takeTheBoat);
+		var cutPainting = new ConditionalStep(this, takeTheBoat);
 		cutPainting.addStep(new Conditions(onIsland, knife), useKnifeOnPainting);
 		cutPainting.addStep(onIsland, takeKnife);
 		steps.put(40, cutPainting);
 
-		ConditionalStep enterRubyRoom = new ConditionalStep(this, takeTheBoat);
+		var enterRubyRoom = new ConditionalStep(this, takeTheBoat);
 		enterRubyRoom.addStep(new Conditions(onIsland, rubyKey), goThroughRubyDoor);
 		enterRubyRoom.addStep(onIsland, searchPainting);
 		steps.put(45, enterRubyRoom);
 
-		ConditionalStep lightCandles = new ConditionalStep(this, takeTheBoat);
+		var lightCandles = new ConditionalStep(this, takeTheBoat);
 		lightCandles.addStep(new Conditions(onIsland, tinderbox, litCandle1, litCandle2, litCandle3), lightCandle4);
 		lightCandles.addStep(new Conditions(onIsland, tinderbox, litCandle1, litCandle2), lightCandle3);
 		lightCandles.addStep(new Conditions(onIsland, tinderbox, litCandle1), lightCandle2);
@@ -128,24 +129,24 @@ public class MisthalinMystery extends BasicQuestHelper
 		lightCandles.addStep(onIsland, takeTinderbox);
 		steps.put(50, lightCandles);
 
-		ConditionalStep lightFuseOnBarrel = new ConditionalStep(this, takeTheBoat);
+		var lightFuseOnBarrel = new ConditionalStep(this, takeTheBoat);
 		lightFuseOnBarrel.addStep(new Conditions(onIsland, tinderbox), lightBarrel);
 		lightFuseOnBarrel.addStep(onIsland, takeTinderbox);
 		steps.put(55, lightFuseOnBarrel);
 		steps.put(60, leaveExplosionRoom);
 
-		ConditionalStep goToLacey = new ConditionalStep(this, takeTheBoat);
+		var goToLacey = new ConditionalStep(this, takeTheBoat);
 		goToLacey.addStep(inOutsideArea, observeThroughTree);
 		goToLacey.addStep(onIsland, climbWall);
 		steps.put(65, goToLacey);
 
-		ConditionalStep pickUpAndReadNotes2 = new ConditionalStep(this, takeTheBoat);
+		var pickUpAndReadNotes2 = new ConditionalStep(this, takeTheBoat);
 		pickUpAndReadNotes2.addStep(notes2, readNotes2);
 		pickUpAndReadNotes2.addStep(inOutsideArea, takeNote2);
 		pickUpAndReadNotes2.addStep(onIsland, climbWall);
 		steps.put(70, pickUpAndReadNotes2);
 
-		ConditionalStep playMusic = new ConditionalStep(this, takeTheBoat);
+		var playMusic = new ConditionalStep(this, takeTheBoat);
 		playMusic.addStep(playedA, playDAgain);
 		playMusic.addStep(playedE, playA);
 		playMusic.addStep(playedD, playE);
@@ -155,28 +156,28 @@ public class MisthalinMystery extends BasicQuestHelper
 		playMusic.addStep(onIsland, climbWall);
 		steps.put(75, playMusic);
 
-		ConditionalStep openingTheEmeraldDoor = new ConditionalStep(this, takeTheBoat);
+		var openingTheEmeraldDoor = new ConditionalStep(this, takeTheBoat);
 		openingTheEmeraldDoor.addStep(new Conditions(inOutsideArea, emeraldKey), returnOverBrokenWall);
 		openingTheEmeraldDoor.addStep(inOutsideArea, searchThePiano);
 		openingTheEmeraldDoor.addStep(new Conditions(onIsland, emeraldKey), openEmeraldDoor);
 		openingTheEmeraldDoor.addStep(onIsland, climbWall);
 		steps.put(80, openingTheEmeraldDoor);
 
-		ConditionalStep enterBandosGodswordRoom = new ConditionalStep(this, takeTheBoat);
+		var enterBandosGodswordRoom = new ConditionalStep(this, takeTheBoat);
 		enterBandosGodswordRoom.addStep(onIsland, enterBandosGodswordRoomStep);
 		steps.put(85, enterBandosGodswordRoom);
 
-		ConditionalStep startPuzzle3 = new ConditionalStep(this, takeTheBoat);
+		var startPuzzle3 = new ConditionalStep(this, takeTheBoat);
 		startPuzzle3.addStep(new Conditions(onIsland, notes3), readNotes3);
 		startPuzzle3.addStep(onIsland, takeNote3);
 		steps.put(90, startPuzzle3);
 
-		ConditionalStep openFireplace = new ConditionalStep(this, takeTheBoat);
+		var openFireplace = new ConditionalStep(this, takeTheBoat);
 		openFireplace.addStep(new Conditions(onIsland, knife), useKnifeOnFireplace);
 		openFireplace.addStep(onIsland, takeKnife);
 		steps.put(95, openFireplace);
 
-		ConditionalStep solveFireplacePuzzle = new ConditionalStep(this, takeTheBoat);
+		var solveFireplacePuzzle = new ConditionalStep(this, takeTheBoat);
 		solveFireplacePuzzle.addStep(selectedOnyx, clickRuby);
 		solveFireplacePuzzle.addStep(selectedEmerald, clickOnyx);
 		solveFireplacePuzzle.addStep(selectedZenyte, clickEmerald);
@@ -187,33 +188,33 @@ public class MisthalinMystery extends BasicQuestHelper
 		solveFireplacePuzzle.addStep(onIsland, searchFireplace);
 		steps.put(100, solveFireplacePuzzle);
 
-		ConditionalStep openSapphireDoor = new ConditionalStep(this, takeTheBoat);
+		var openSapphireDoor = new ConditionalStep(this, takeTheBoat);
 		openSapphireDoor.addStep(new Conditions(onIsland, sapphireKey), goThroughSapphireDoor);
 		openSapphireDoor.addStep(onIsland, searchFireplaceForSapphireKey);
 		steps.put(105, openSapphireDoor);
 
-		ConditionalStep goDoBoss = new ConditionalStep(this, takeTheBoat);
+		var goDoBoss = new ConditionalStep(this, takeTheBoat);
 		goDoBoss.addStep(inBossRoom, reflectKnives);
 		goDoBoss.addStep(onIsland, goThroughSapphireDoor);
 		steps.put(110, goDoBoss);
 		steps.put(111, goDoBoss);
 
-		ConditionalStep watchRevealCutscene = new ConditionalStep(this, takeTheBoat);
+		var watchRevealCutscene = new ConditionalStep(this, takeTheBoat);
 		watchRevealCutscene.addStep(inBossRoom, watchTheKillersReveal);
 		watchRevealCutscene.addStep(onIsland, continueThroughSapphireDoor);
 		steps.put(115, watchRevealCutscene);
 
-		ConditionalStep goFightAbigale = new ConditionalStep(this, takeTheBoat);
+		var goFightAbigale = new ConditionalStep(this, takeTheBoat);
 		goFightAbigale.addStep(new Conditions(inBossRoom, killersKnife), fightAbigale);
 		goFightAbigale.addStep(inBossRoom, pickUpKillersKnife);
 		goFightAbigale.addStep(onIsland, continueThroughSapphireDoor);
 		steps.put(120, goFightAbigale);
 
-		ConditionalStep attemptToLeaveSapphireRoom = new ConditionalStep(this, takeTheBoat);
+		var attemptToLeaveSapphireRoom = new ConditionalStep(this, takeTheBoat);
 		attemptToLeaveSapphireRoom.addStep(onIsland, leaveSapphireRoom);
 		steps.put(125, attemptToLeaveSapphireRoom);
 
-		ConditionalStep finishTheQuest = new ConditionalStep(this, takeTheBoat);
+		var finishTheQuest = new ConditionalStep(this, takeTheBoat);
 		finishTheQuest.addStep(onIsland, talkToMandy);
 		steps.put(130, finishTheQuest);
 
@@ -378,22 +379,25 @@ public class MisthalinMystery extends BasicQuestHelper
 	@Override
 	public List<ExperienceReward> getExperienceRewards()
 	{
-		return Collections.singletonList(new ExperienceReward(Skill.CRAFTING, 600));
+		return List.of(
+			new ExperienceReward(Skill.CRAFTING, 600)
+		);
 	}
 
 	@Override
 	public List<ItemReward> getItemRewards()
 	{
-		return Arrays.asList(
+		return List.of(
 			new ItemReward("Uncut Ruby", ItemID.UNCUT_RUBY, 1),
 			new ItemReward("Uncut Emerald", ItemID.UNCUT_EMERALD, 1),
-			new ItemReward("Uncut Sapphire", ItemID.UNCUT_SAPPHIRE, 1));
+			new ItemReward("Uncut Sapphire", ItemID.UNCUT_SAPPHIRE, 1)
+		);
 	}
 
 	@Override
 	public List<PanelDetails> getPanels()
 	{
-		List<PanelDetails> allSteps = new ArrayList<>();
+		var allSteps = new ArrayList<PanelDetails>();
 
 		allSteps.add(new PanelDetails("Talk to Abigale", Collections.singletonList(talkToAbigale)));
 		allSteps.add(new PanelDetails("Enter the manor", Arrays.asList(takeTheBoat, takeTheBucket, searchTheBarrel, useBucketOnBarrel, searchTheBarrelForKey, openManorDoor)));
@@ -403,6 +407,7 @@ public class MisthalinMystery extends BasicQuestHelper
 		allSteps.add(new PanelDetails("Witness another murder", Arrays.asList(returnOverBrokenWall, openEmeraldDoor, enterBandosGodswordRoomStep)));
 		allSteps.add(new PanelDetails("Solve the fourth puzzle", Arrays.asList(takeNote3, readNotes3, useKnifeOnFireplace, searchFireplace, clickSapphire, clickDiamond, clickZenyte, clickEmerald, clickOnyx, clickRuby, searchFireplaceForSapphireKey)));
 		allSteps.add(new PanelDetails("Confront the killer", Arrays.asList(goThroughSapphireDoor, reflectKnives, watchTheKillersReveal, pickUpKillersKnife, fightAbigale, leaveSapphireRoom, talkToMandy)));
+
 		return allSteps;
 	}
 }

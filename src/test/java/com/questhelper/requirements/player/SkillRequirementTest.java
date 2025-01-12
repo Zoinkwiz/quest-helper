e21b121622692458bce7c7a60f28d315ffc22aff
pajlada <rasmus.karlsson@pajlada.com>
2025-01-12 14:12:30 +0100

    fix: ensure SkillRequirement checks doesn't try to stack boosts (#1938)
    * add tests (that will fail!!)
    
    * fix: ensure we don't try to stack boosts

---
+++ src/test/java/com/questhelper/requirements/player/SkillRequirementTest.java
@@ -0,0 +1,61 @@
+package com.questhelper.requirements.player;
+
+import com.questhelper.MockedTest;
+import net.runelite.api.Skill;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
+
+public class SkillRequirementTest extends MockedTest
+{
+	void initFishing(int realLevel, int boostedLevel) {
+		when(client.getRealSkillLevel(Skill.FISHING)).thenReturn(realLevel);
+		when(client.getBoostedSkillLevel(Skill.FISHING)).thenReturn(boostedLevel);
+	}
+
+	@Test
+	void unboostable() {
+		var req = new SkillRequirement(Skill.FISHING, 60);
+
+		initFishing(60, 60);
+		assertEquals(SkillRequirement.BoostStatus.Pass, req.checkBoosted(client, questHelperConfig));
+		assertTrue(req.check(client));
+
+		// User has temporarily lost some stats
+		initFishing(60, 59);
+		assertEquals(SkillRequirement.BoostStatus.Pass, req.checkBoosted(client, questHelperConfig));
+		assertTrue(req.check(client));
+
+		initFishing(59, 59);
+		assertEquals(SkillRequirement.BoostStatus.Fail, req.checkBoosted(client, questHelperConfig));
+		assertFalse(req.check(client));
+
+		// User has boosted, but this requirement doesn't allow boosting
+		initFishing(59, 61);
+		assertEquals(SkillRequirement.BoostStatus.Fail, req.checkBoosted(client, questHelperConfig));
+		assertFalse(req.check(client));
+	}
+
+	@Test
+	void boostable() {
+		var req = new SkillRequirement(Skill.FISHING, 60, true);
+
+		// User is outside of boost range
+		initFishing(54, 54);
+		assertEquals(SkillRequirement.BoostStatus.Fail, req.checkBoosted(client, questHelperConfig));
+
+		// User is outside of boost range
+		initFishing(54, 59);
+		assertEquals(SkillRequirement.BoostStatus.Fail, req.checkBoosted(client, questHelperConfig));
+
+		// User can boost
+		initFishing(57, 57);
+		assertEquals(SkillRequirement.BoostStatus.CanPassWithBoost, req.checkBoosted(client, questHelperConfig));
+
+		// User has already boosted
+		initFishing(57, 62);
+		assertEquals(SkillRequirement.BoostStatus.Pass, req.checkBoosted(client, questHelperConfig));
+	}
+}

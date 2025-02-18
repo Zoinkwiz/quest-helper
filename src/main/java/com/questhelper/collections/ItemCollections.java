f20a4a270a3a43a497793cf98ff276b985a2ffc6
Cooper Morris <coopermorris96@yahoo.com>
2025-02-18 11:41:17 -0800

    Allow volcanic mine prospector helm to be used for Falador Hard diary (#1979)
    * Allow volcanic mine prospector helm to be used for Falador Hard diary
    
    * Add golden prospector helmet

--- src/main/java/com/questhelper/collections/ItemCollections.java
+++ src/main/java/com/questhelper/collections/ItemCollections.java
@@ -2184,6 +2184,12 @@ public enum ItemCollections
 		ItemID.GHOMMALS_HILT_3,
 		ItemID.GHOMMALS_HILT_2,
 		ItemID.GHOMMALS_HILT_1
+	)),
+	
+	PROSPECTOR_HELMET(ImmutableList.of(
+		ItemID.PROSPECTOR_HELMET,
+		ItemID.PROSPECTOR_HELMET_29472,
+		ItemID.GOLDEN_PROSPECTOR_HELMET
 	));
 
 	private final List<Integer> items;

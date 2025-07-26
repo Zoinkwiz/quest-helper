1c879fdbaf4baf85a6f50b62c3f6a5ee082b87b3
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2025-07-26 14:13:51 +0200

    clean up egg solver & some texts

--- src/main/java/com/questhelper/helpers/quests/scrambled/EggSolver.java
+++ src/main/java/com/questhelper/helpers/quests/scrambled/EggSolver.java
@@ -1,33 +1,60 @@
 package com.questhelper.helpers.quests.scrambled;
 
-import com.google.inject.Inject;
 import com.questhelper.QuestHelperPlugin;
 import com.questhelper.questhelpers.QuestHelper;
-import com.questhelper.requirements.widget.WidgetPresenceRequirement;
+import com.questhelper.requirements.SimpleRequirement;
 import com.questhelper.steps.ConditionalStep;
 import com.questhelper.steps.DetailedOwnerStep;
 import com.questhelper.steps.DetailedQuestStep;
 import com.questhelper.steps.QuestStep;
-import com.questhelper.steps.WidgetStep;
 import com.questhelper.steps.widget.WidgetHighlight;
-import lombok.extern.slf4j.Slf4j;
-import net.runelite.api.Client;
-import net.runelite.api.widgets.Widget;
-import javax.annotation.Nullable;
 import java.awt.*;
 import java.util.List;
+import javax.annotation.Nullable;
+import lombok.extern.slf4j.Slf4j;
+import net.runelite.api.Client;
+import net.runelite.api.gameval.InterfaceID;
+import net.runelite.api.widgets.Widget;
 
 @Slf4j
 public class EggSolver extends DetailedOwnerStep
 {
-	@Inject
-	Client client;
 
 	private ConditionalStep solvePuzzle;
+
 
 	public EggSolver(Scrambled parentHelper)
 	{
 		super(parentHelper, "Solve the puzzle by following the instructions in the overlay.");
+	}
+
+	/**
+	 * Find the widget with the given interface ID and, if it exists, look through its dynamic children for a widget
+	 * matching the given model ID
+	 */
+	private static @Nullable Widget findWidgetByModelID(Client client, int interfaceID, int modelID)
+	{
+		var widget = client.getWidget(interfaceID);
+
+		if (widget == null)
+		{
+			return null;
+		}
+
+		var children = widget.getChildren();
+
+		if (children != null)
+		{
+			for (var w : children)
+			{
+				if (w != null && w.getModelId() == modelID)
+				{
+					return w;
+				}
+			}
+		}
+
+		return null;
 	}
 
 	@Override
@@ -40,37 +67,36 @@ public class EggSolver extends DetailedOwnerStep
 	protected void setupSteps()
 	{
 		var h = getQuestHelper();
-		var unreachable = new DetailedQuestStep(h, "this step should be unreachable, info for an egg piece is missing!");
+		var unreachable = new DetailedQuestStep(h, "Something went wrong with the puzzle solver, please report this in the Quest Helper Discord with a screenshot of your puzzle.");
 
 		solvePuzzle = new ConditionalStep(getQuestHelper(), unreachable);
 
-		addEggPair(solvePuzzle, 57101, 0, 165, 141);
-		addEggPair(solvePuzzle, 57102, 0, 297, 217);
-		addEggPair(solvePuzzle, 57103, 0/*todo: confirm*/, 243, 149);
-		addEggPair(solvePuzzle, 57104, 0/*todo confirm*/, 231, 71);
-		addEggPair(solvePuzzle, 57105, 0, 149, 179);
-		addEggPair(solvePuzzle, 57106, 0 /* todo confirm */, 194, 121);
-		addEggPair(solvePuzzle, 57107, 0, 250, 41);
-		addEggPair(solvePuzzle, 57108, 0, 209, 160);
-		addEggPair(solvePuzzle, 57109, 0, 217, 27);
-		addEggPair(solvePuzzle, 57110, 0, 157, 103);
-		addEggPair(solvePuzzle, 57111, 0, 260, 22);
-		addEggPair(solvePuzzle, 57112, 0, 199, 248);
-		// 57113 doesn't exist?
-		addEggPair(solvePuzzle, 57114, 0, 269, 177);
-		addEggPair(solvePuzzle, 57115, 0, 257, 113);
-		addEggPair(solvePuzzle, 57116, 0, 306, 119);
-		addEggPair(solvePuzzle, 57117, 0, 196, 105);
-		addEggPair(solvePuzzle, 57118, 0, 288, 71);
-		addEggPair(solvePuzzle, 57119, 0, 187, 210);
-		addEggPair(solvePuzzle, 57120, 0, 309, 160);
-		addEggPair(solvePuzzle, 57121, 0, 222, 199);
-		addEggPair(solvePuzzle, 57122, 0, 273, 115);
-		addEggPair(solvePuzzle, 57123, 0, 175, 58);
-		addEggPair(solvePuzzle, 57124, 0, 252, 213);
-		addEggPair(solvePuzzle, 57125, 0, 198, 60);
-		addEggPair(solvePuzzle, 57126, 0, 244, 255);
-		addEggPair(solvePuzzle, 57127, 0, 231, 99);
+		addEggPair(solvePuzzle, 57101, 165, 141);
+		addEggPair(solvePuzzle, 57102, 297, 217);
+		addEggPair(solvePuzzle, 57103, 243, 149);
+		addEggPair(solvePuzzle, 57104, 231, 71);
+		addEggPair(solvePuzzle, 57105, 149, 179);
+		addEggPair(solvePuzzle, 57106, 194, 121);
+		addEggPair(solvePuzzle, 57107, 250, 41);
+		addEggPair(solvePuzzle, 57108, 209, 160);
+		addEggPair(solvePuzzle, 57109, 217, 27);
+		addEggPair(solvePuzzle, 57110, 157, 103);
+		addEggPair(solvePuzzle, 57111, 260, 22);
+		addEggPair(solvePuzzle, 57112, 199, 248);
+		addEggPair(solvePuzzle, 57114, 269, 177);
+		addEggPair(solvePuzzle, 57115, 257, 113);
+		addEggPair(solvePuzzle, 57116, 306, 119);
+		addEggPair(solvePuzzle, 57117, 196, 105);
+		addEggPair(solvePuzzle, 57118, 288, 71);
+		addEggPair(solvePuzzle, 57119, 187, 210);
+		addEggPair(solvePuzzle, 57120, 309, 160);
+		addEggPair(solvePuzzle, 57121, 222, 199);
+		addEggPair(solvePuzzle, 57122, 273, 115);
+		addEggPair(solvePuzzle, 57123, 175, 58);
+		addEggPair(solvePuzzle, 57124, 252, 213);
+		addEggPair(solvePuzzle, 57125, 198, 60);
+		addEggPair(solvePuzzle, 57126, 244, 255);
+		addEggPair(solvePuzzle, 57127, 231, 99);
 	}
 
 	protected void updateSteps()
@@ -86,33 +112,39 @@ public class EggSolver extends DetailedOwnerStep
 		);
 	}
 
-	// TODO: should also accept a position & required rotation
-	private void addEggPair(ConditionalStep c, int modelID, int requiredRotation, int positionX, int positionY)
+	private void addEggPair(ConditionalStep c, int modelID, int positionX, int positionY)
 	{
-		var step = new EggPieceStep(getQuestHelper(), modelID, requiredRotation, positionX, positionY);
+		var step = new EggPieceStep(getQuestHelper(), modelID, positionX, positionY);
 		var requirement = new EggPieceRequirement(modelID);
 
 		c.addStep(requirement, step);
 	}
 
-	private class EggPieceStep extends QuestStep
+	private static class EggPieceStep extends QuestStep
 	{
-		int modelID;
-		int requiredRotation;
+		/// The rotationY required for the widget to be considered correctly rotated
+		private static final int REQUIRED_ROTATION = 0;
+		/// The width of the square we mark for the correct spot
+		private static final int SPOT_WIDTH = 50;
+		/// The height of the square we mark for the correct spot
+		private static final int SPOT_HEIGHT = 50;
 
+		/// The model ID of the egg piece this step is handling
+		int modelID;
+
+		/// The final correct X position for this egg piece
 		int positionX;
+		/// The final correct Y position for this egg piece
 		int positionY;
 
-		public EggPieceStep(QuestHelper questHelper, int modelID, int requiredRotation, int positionX, int positionY)
+		public EggPieceStep(QuestHelper questHelper, int modelID, int positionX, int positionY)
 		{
-			super(questHelper, "Move the highlighted piece");
-			// TODO: required rotation
-			var modelHighlighter = new WidgetHighlight(922, 7, true);
+			super(questHelper, "Rotate the piece until you're prompted to move it to the correct spot. The square of the egg piece and the square marking the correct spot need to roughly line up.");
+			var modelHighlighter = new WidgetHighlight(InterfaceID.Jigsaw.PIECES, true);
 			modelHighlighter.setModelIdRequirement(57106);
 			this.addWidgetHighlight(modelHighlighter);
 
 			this.modelID = modelID;
-			this.requiredRotation = requiredRotation;
 			this.positionX = positionX;
 			this.positionY = positionY;
 		}
@@ -121,105 +153,52 @@ public class EggSolver extends DetailedOwnerStep
 		public void makeWidgetOverlayHint(Graphics2D graphics, QuestHelperPlugin plugin)
 		{
 			super.makeWidgetOverlayHint(graphics, plugin);
-			var root = client.getWidget(922, 0);
-			if (root == null) {
+			var root = client.getWidget(InterfaceID.Jigsaw.UNIVERSE);
+			if (root == null)
+			{
 				return;
 			}
 			var rootBounds = root.getBounds();
 
-			var w = this.getWidget(client);
-			if (w == null) {return;}
+			var w = EggSolver.findWidgetByModelID(client, InterfaceID.Jigsaw.PIECES, modelID);
+			if (w == null)
+			{
+				return;
+			}
 
-			graphics.setColor(Color.CYAN);
+			// NOTE: We could hardcode Cyan here
+			graphics.setColor(getQuestHelper().getConfig().targetOverlayColor());
 
 			graphics.drawRect(w.getBounds().x, w.getBounds().y, w.getBounds().width, w.getBounds().height);
 
-			graphics.drawString(String.format("%d", w.getRotationY()), w.getBounds().x, w.getBounds().y + w.getBounds().height);
-
-			if (w.getRotationY() == requiredRotation) {
+			if (w.getRotationY() == REQUIRED_ROTATION)
+			{
 				graphics.drawString("move here", rootBounds.x + this.positionX, rootBounds.y + this.positionY);
-				graphics.drawRect(rootBounds.x + this.positionX, rootBounds.y + this.positionY, 50, 50);
-			} else {
-				// TODO: rotate how many times?
+				graphics.drawRect(rootBounds.x + this.positionX, rootBounds.y + this.positionY, SPOT_WIDTH, SPOT_HEIGHT);
+			}
+			else
+			{
+				// TODO: We could technically prompt the user with exactly how many times to click the egg piece, but that's overkill imo
 				graphics.drawString("rotate", w.getBounds().x, w.getBounds().y);
 			}
 
 		}
-
-		@Nullable
-		protected Widget getWidget(Client client)
-		{
-			var widget = client.getWidget(922, 7);
-
-			if (widget == null)
-			{
-				return null;
-			}
-
-			var children = widget.getChildren();
-			var staticChildren = widget.getStaticChildren();
-
-			// TODO: do we need to check static widgets AND dynamic widgets?
-			if (children != null)
-			{
-				for (var w : children)
-				{
-					if (w != null && w.getModelId() == modelID) {
-						return w;
-					}
-				}
-				// for (var w : staticChildren)
-				// {
-				// 	if (w != null && w.getModelId() == modelID) {
-				// 		return w;
-				// 	}
-				// }
-			}
-
-			return null;
-		}
 	}
 
-	private class EggPieceRequirement extends WidgetPresenceRequirement
+	private static class EggPieceRequirement extends SimpleRequirement
 	{
-		private int modelID;
-		public EggPieceRequirement(int modelID) {
-			super(922, 7);
+		private final int modelID;
+
+		public EggPieceRequirement(int modelID)
+		{
 			this.modelID = modelID;
 		}
 
 		@Override
-		@Nullable
-		protected Widget getWidget(Client client)
+		public boolean check(Client client)
 		{
-			var widget = client.getWidget(groupId, childId);
-
-			if (widget == null)
-			{
-				return null;
-			}
-
-			var children = widget.getChildren();
-			var staticChildren = widget.getStaticChildren();
-
-			// TODO: do we need to check static widgets AND dynamic widgets?
-			if (children != null)
-			{
-				for (var w : children)
-				{
-					if (w != null && w.getModelId() == modelID) {
-						return w;
-					}
-				}
-				// for (var w : staticChildren)
-				// {
-				// 	if (w != null && w.getModelId() == modelID) {
-				// 		return w;
-				// 	}
-				// }
-			}
-
-			return null;
+			var w = EggSolver.findWidgetByModelID(client, InterfaceID.Jigsaw.PIECES, modelID);
+			return w != null;
 		}
 	}
 }

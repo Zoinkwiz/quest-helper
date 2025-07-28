0913ec5e5d5ac0bc8f319c59c009af21562ce4d5
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2025-07-28 21:27:23 +0200

    chore: add myself to the copyright section

--- src/main/java/com/questhelper/helpers/mischelpers/strongholdofsecurity/StrongholdOfSecurity.java
+++ src/main/java/com/questhelper/helpers/mischelpers/strongholdofsecurity/StrongholdOfSecurity.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2023, Okke234
+ * Copyright (c) 2025, pajlada <https://github.com/pajlada>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without

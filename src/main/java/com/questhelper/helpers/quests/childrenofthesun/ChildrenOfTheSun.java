31ab5aa48b995c499369e306be5613ff5dd4e096
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2025-08-05 23:08:08 +0200

    refactor: modernize
    We are purposefully not using List.of in the line points since it does not accept null values.

--- src/main/java/com/questhelper/helpers/quests/childrenofthesun/ChildrenOfTheSun.java
+++ src/main/java/com/questhelper/helpers/quests/childrenofthesun/ChildrenOfTheSun.java
@@ -27,35 +27,31 @@ package com.questhelper.helpers.quests.childrenofthesun;
 import com.questhelper.panel.PanelDetails;
 import com.questhelper.questhelpers.BasicQuestHelper;
 import com.questhelper.requirements.Requirement;
-import com.questhelper.requirements.item.ItemRequirement;
 import com.questhelper.requirements.npc.NpcRequirement;
+import static com.questhelper.requirements.util.LogicHelper.and;
 import com.questhelper.requirements.var.VarbitRequirement;
 import com.questhelper.requirements.zone.Zone;
 import com.questhelper.requirements.zone.ZoneRequirement;
 import com.questhelper.rewards.QuestPointReward;
 import com.questhelper.rewards.UnlockReward;
-import com.questhelper.steps.*;
+import com.questhelper.steps.ConditionalStep;
+import com.questhelper.steps.DetailedQuestStep;
+import com.questhelper.steps.MultiNpcStep;
+import com.questhelper.steps.NpcStep;
+import com.questhelper.steps.ObjectStep;
+import com.questhelper.steps.PuzzleWrapperStep;
+import com.questhelper.steps.QuestStep;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import net.runelite.api.coords.WorldPoint;
 import net.runelite.api.gameval.NpcID;
 import net.runelite.api.gameval.ObjectID;
 
-import java.util.*;
-
-import static com.questhelper.requirements.util.LogicHelper.and;
-
 public class ChildrenOfTheSun extends BasicQuestHelper
 {
-
-	DetailedQuestStep talkToAlina, followGuard, attemptToEnterHouse, talkToTobyn,
-		reportBackToTobyn, goUpVarrockF0ToF1, goUpVarrockF1toF2, finishQuest;
-
-	PuzzleWrapperStep followGuardPuzzleWrapper, markGuard1, markGuard2, markGuard3, markGuard4, unmarkWrongGuard1, unmarkWrongGuard2, unmarkWrongGuard3, unmarkWrongGuard4,
-		unmarkWrongGuard5, unmarkWrongGuard6;
-
-	Zone castleF1, castleF2;
-
-	Requirement isFollowing, markedGuard1, markedGuard2, markedGuard3, markedGuard4, markedWrongGuard1,
-		markedWrongGuard2, markedWrongGuard3, markedWrongGuard4, markedWrongGuard5, markedWrongGuard6, inCastleF1, inCastleF2;
 
 	final int GUARD_1_CHANGE_VARBIT = 9633;
 	final int GUARD_2_CHANGE_VARBIT = 9634;
@@ -68,53 +64,55 @@ public class ChildrenOfTheSun extends BasicQuestHelper
 	final int WRONG_GUARD_5_CHANGE_VARBIT = 9643;
 	final int WRONG_GUARD_6_CHANGE_VARBIT = 9644;
 
-	@Override
-	public Map<Integer, QuestStep> loadSteps()
-	{
-		initializeRequirements();
-		setupConditions();
-		setupSteps();
-		Map<Integer, QuestStep> steps = new HashMap<>();
+	// Zones
+	Zone castleF1;
+	Zone castleF2;
 
-		// Before start, 9646 0->1, talked a bit to Alina
-		steps.put(0, talkToAlina);
-		steps.put(2, talkToAlina);
-		steps.put(4, talkToAlina);
-		ConditionalStep goFollowGuard = new ConditionalStep(this, talkToAlina);
-		goFollowGuard.addStep(isFollowing, followGuardPuzzleWrapper);
-		steps.put(6, goFollowGuard);
-		steps.put(8, attemptToEnterHouse);
-		steps.put(10, talkToTobyn);
-		ConditionalStep markGuards = new ConditionalStep(this, markGuard1);
-		markGuards.addStep(markedWrongGuard1, unmarkWrongGuard1);
-		markGuards.addStep(markedWrongGuard2, unmarkWrongGuard2);
-		markGuards.addStep(markedWrongGuard3, unmarkWrongGuard3);
-		markGuards.addStep(markedWrongGuard4, unmarkWrongGuard4);
-		markGuards.addStep(markedWrongGuard5, unmarkWrongGuard5);
-		markGuards.addStep(markedWrongGuard6, unmarkWrongGuard6);
-		markGuards.addStep(and(markedGuard1, markedGuard2, markedGuard3, markedGuard4), reportBackToTobyn);
-		markGuards.addStep(and(markedGuard1, markedGuard2, markedGuard4), markGuard3);
-		markGuards.addStep(and(markedGuard1, markedGuard2), markGuard4);
-		markGuards.addStep(markedGuard1, markGuard2);
-		steps.put(12, markGuards);
-		steps.put(14, reportBackToTobyn);
-		ConditionalStep goFinishQuest = new ConditionalStep(this, goUpVarrockF0ToF1);
-		goFinishQuest.addStep(inCastleF2, finishQuest);
-		goFinishQuest.addStep(inCastleF1, goUpVarrockF1toF2);
-		steps.put(16, goFinishQuest);
-		steps.put(18, goFinishQuest);
-		steps.put(20, goFinishQuest);
-		steps.put(22, goFinishQuest);
-		return steps;
+	// Miscellaneous requirements
+	Requirement isFollowing;
+	Requirement markedGuard1;
+	Requirement markedGuard2;
+	Requirement markedGuard3;
+	Requirement markedGuard4;
+	Requirement markedWrongGuard1;
+	Requirement markedWrongGuard2;
+	Requirement markedWrongGuard3;
+	Requirement markedWrongGuard4;
+	Requirement markedWrongGuard5;
+	Requirement markedWrongGuard6;
+	Requirement inCastleF1;
+	Requirement inCastleF2;
+
+	// Steps
+	DetailedQuestStep talkToAlina;
+	DetailedQuestStep followGuard;
+	DetailedQuestStep attemptToEnterHouse;
+	DetailedQuestStep talkToTobyn;
+	DetailedQuestStep reportBackToTobyn;
+	DetailedQuestStep goUpVarrockF0ToF1;
+	DetailedQuestStep goUpVarrockF1toF2;
+	DetailedQuestStep finishQuest;
+	PuzzleWrapperStep followGuardPuzzleWrapper;
+	PuzzleWrapperStep markGuard1;
+	PuzzleWrapperStep markGuard2;
+	PuzzleWrapperStep markGuard3;
+	PuzzleWrapperStep markGuard4;
+	PuzzleWrapperStep unmarkWrongGuard1;
+	PuzzleWrapperStep unmarkWrongGuard2;
+	PuzzleWrapperStep unmarkWrongGuard3;
+	PuzzleWrapperStep unmarkWrongGuard4;
+	PuzzleWrapperStep unmarkWrongGuard5;
+	PuzzleWrapperStep unmarkWrongGuard6;
+
+	@Override
+	protected void setupZones()
+	{
+		castleF1 = new Zone(new WorldPoint(3199, 3465, 1), new WorldPoint(3227, 3500, 1));
+		castleF2 = new Zone(new WorldPoint(3199, 3465, 2), new WorldPoint(3227, 3500, 2));
 	}
 
 	@Override
 	protected void setupRequirements()
-	{
-
-	}
-
-	public void setupConditions()
 	{
 		isFollowing = new NpcRequirement("Guard", NpcID.VMQ1_BAG_GUARD);
 		markedGuard1 = new VarbitRequirement(GUARD_1_CHANGE_VARBIT, 2);
@@ -129,8 +127,6 @@ public class ChildrenOfTheSun extends BasicQuestHelper
 		markedWrongGuard5 = new VarbitRequirement(WRONG_GUARD_5_CHANGE_VARBIT, 2);
 		markedWrongGuard6 = new VarbitRequirement(WRONG_GUARD_6_CHANGE_VARBIT, 2);
 
-		castleF1 = new Zone(new WorldPoint(3199, 3465, 1), new WorldPoint(3227, 3500, 1));
-		castleF2 = new Zone(new WorldPoint(3199, 3465, 2), new WorldPoint(3227, 3500, 2));
 		inCastleF1 = new ZoneRequirement(castleF1);
 		inCastleF2 = new ZoneRequirement(castleF2);
 	}
@@ -179,11 +175,11 @@ public class ChildrenOfTheSun extends BasicQuestHelper
 		talkToTobyn = new NpcStep(this, NpcID.VMQ1_GUARD_SERGEANT_VIS, new WorldPoint(3211, 3437, 0), "Talk to Sergeant Tobyn in Varrock Square.");
 		markGuard1 = new PuzzleWrapperStep(this,
 			new MultiNpcStep(this, NpcID.VMQ1_GUARD_1_UNMARKED, new WorldPoint(3208, 3422, 0),
-			"Mark the guard outside Aris's tent.", GUARD_1_CHANGE_VARBIT, BASE_GUARD_ID),
+				"Mark the guard outside Aris's tent.", GUARD_1_CHANGE_VARBIT, BASE_GUARD_ID),
 			"Mark the suspect guards.");
 		markGuard2 = new PuzzleWrapperStep(this,
 			new MultiNpcStep(this, NpcID.VMQ1_GUARD_2_UNMARKED, new WorldPoint(3221, 3430, 0),
-			"Mark the guard south east of Benny's news stand, who isn't wearing a helmet and has long hair.", GUARD_1_CHANGE_VARBIT, BASE_GUARD_ID),
+				"Mark the guard south east of Benny's news stand, who isn't wearing a helmet and has long hair.", GUARD_1_CHANGE_VARBIT, BASE_GUARD_ID),
 			"Mark the suspect guards.").withNoHelpHiddenInSidebar(true);
 		markGuard3 = new PuzzleWrapperStep(this,
 			new MultiNpcStep(this, NpcID.VMQ1_GUARD_3_UNMARKED, new WorldPoint(3246, 3429, 0),
@@ -196,27 +192,27 @@ public class ChildrenOfTheSun extends BasicQuestHelper
 
 		unmarkWrongGuard1 = new PuzzleWrapperStep(this,
 			new MultiNpcStep(this, NpcID.VMQ1_GUARD_5_MARKED, new WorldPoint(3227, 3424, 0),
-			"Unmark the guard east of ELiza.", GUARD_1_CHANGE_VARBIT, BASE_GUARD_ID),
+				"Unmark the guard east of ELiza.", GUARD_1_CHANGE_VARBIT, BASE_GUARD_ID),
 			"Mark the suspect guards.");
 		unmarkWrongGuard2 = new PuzzleWrapperStep(this,
 			new MultiNpcStep(this, NpcID.VMQ1_GUARD_6_MARKED, new WorldPoint(3218, 3424, 0),
-			"Unmark the guard next to Eliza.", GUARD_1_CHANGE_VARBIT, BASE_GUARD_ID),
+				"Unmark the guard next to Eliza.", GUARD_1_CHANGE_VARBIT, BASE_GUARD_ID),
 			"Mark the suspect guards.");
 		unmarkWrongGuard3 = new PuzzleWrapperStep(this,
 			new MultiNpcStep(this, NpcID.VMQ1_GUARD_7_MARKED, new WorldPoint(3230, 3430, 0),
-			"Unmark the guard roaming south of Horvik.", GUARD_1_CHANGE_VARBIT, BASE_GUARD_ID),
+				"Unmark the guard roaming south of Horvik.", GUARD_1_CHANGE_VARBIT, BASE_GUARD_ID),
 			"Mark the suspect guards.");
 		unmarkWrongGuard4 = new PuzzleWrapperStep(this,
 			new MultiNpcStep(this, NpcID.VMQ1_GUARD_8_MARKED, new WorldPoint(3206, 3431, 0),
-			"Unmark the guard outside Zaff's Superior Staffs.", GUARD_1_CHANGE_VARBIT, BASE_GUARD_ID),
+				"Unmark the guard outside Zaff's Superior Staffs.", GUARD_1_CHANGE_VARBIT, BASE_GUARD_ID),
 			"Mark the suspect guards.");
 		unmarkWrongGuard5 = new PuzzleWrapperStep(this,
 			new MultiNpcStep(this, NpcID.VMQ1_GUARD_9_MARKED, new WorldPoint(3239, 3433, 0),
-			"Unmark the guard next to the small fountain east of Horvik.", GUARD_1_CHANGE_VARBIT, BASE_GUARD_ID),
+				"Unmark the guard next to the small fountain east of Horvik.", GUARD_1_CHANGE_VARBIT, BASE_GUARD_ID),
 			"Mark the suspect guards.");
 		unmarkWrongGuard6 = new PuzzleWrapperStep(this,
 			new MultiNpcStep(this, NpcID.VMQ1_GUARD_10_MARKED, new WorldPoint(3218, 3433, 0),
-			"Unmark the guard next to Baraek.", GUARD_1_CHANGE_VARBIT, BASE_GUARD_ID),
+				"Unmark the guard next to Baraek.", GUARD_1_CHANGE_VARBIT, BASE_GUARD_ID),
 			"Mark the suspect guards.");
 
 		reportBackToTobyn = new NpcStep(this, NpcID.VMQ1_GUARD_SERGEANT_VIS, new WorldPoint(3211, 3437, 0),
@@ -231,15 +227,44 @@ public class ChildrenOfTheSun extends BasicQuestHelper
 	}
 
 	@Override
-	public List<ItemRequirement> getItemRequirements()
+	public Map<Integer, QuestStep> loadSteps()
 	{
-		return null;
-	}
+		initializeRequirements();
+		setupSteps();
 
-	@Override
-	public List<ItemRequirement> getItemRecommended()
-	{
-		return null;
+		var steps = new HashMap<Integer, QuestStep>();
+
+		// Before start, 9646 0->1, talked a bit to Alina
+		steps.put(0, talkToAlina);
+		steps.put(2, talkToAlina);
+		steps.put(4, talkToAlina);
+		var goFollowGuard = new ConditionalStep(this, talkToAlina);
+		goFollowGuard.addStep(isFollowing, followGuardPuzzleWrapper);
+		steps.put(6, goFollowGuard);
+		steps.put(8, attemptToEnterHouse);
+		steps.put(10, talkToTobyn);
+		var markGuards = new ConditionalStep(this, markGuard1);
+		markGuards.addStep(markedWrongGuard1, unmarkWrongGuard1);
+		markGuards.addStep(markedWrongGuard2, unmarkWrongGuard2);
+		markGuards.addStep(markedWrongGuard3, unmarkWrongGuard3);
+		markGuards.addStep(markedWrongGuard4, unmarkWrongGuard4);
+		markGuards.addStep(markedWrongGuard5, unmarkWrongGuard5);
+		markGuards.addStep(markedWrongGuard6, unmarkWrongGuard6);
+		markGuards.addStep(and(markedGuard1, markedGuard2, markedGuard3, markedGuard4), reportBackToTobyn);
+		markGuards.addStep(and(markedGuard1, markedGuard2, markedGuard4), markGuard3);
+		markGuards.addStep(and(markedGuard1, markedGuard2), markGuard4);
+		markGuards.addStep(markedGuard1, markGuard2);
+		steps.put(12, markGuards);
+		steps.put(14, reportBackToTobyn);
+		var goFinishQuest = new ConditionalStep(this, goUpVarrockF0ToF1);
+		goFinishQuest.addStep(inCastleF2, finishQuest);
+		goFinishQuest.addStep(inCastleF1, goUpVarrockF1toF2);
+		steps.put(16, goFinishQuest);
+		steps.put(18, goFinishQuest);
+		steps.put(20, goFinishQuest);
+		steps.put(22, goFinishQuest);
+
+		return steps;
 	}
 
 	@Override
@@ -259,9 +284,21 @@ public class ChildrenOfTheSun extends BasicQuestHelper
 	@Override
 	public List<PanelDetails> getPanels()
 	{
-		List<PanelDetails> allSteps = new ArrayList<>();
-		allSteps.add(new PanelDetails("Intrigue", Arrays.asList(talkToAlina, followGuardPuzzleWrapper,
-			attemptToEnterHouse, talkToTobyn, markGuard1, markGuard2, markGuard4, markGuard3, reportBackToTobyn, finishQuest)));
+		var allSteps = new ArrayList<PanelDetails>();
+
+		allSteps.add(new PanelDetails("Intrigue", List.of(
+			talkToAlina,
+			followGuardPuzzleWrapper,
+			attemptToEnterHouse,
+			talkToTobyn,
+			markGuard1,
+			markGuard2,
+			markGuard4,
+			markGuard3,
+			reportBackToTobyn,
+			finishQuest
+		)));
+
 		return allSteps;
 	}
 }

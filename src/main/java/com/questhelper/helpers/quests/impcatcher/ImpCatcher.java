10b1176f9c52e529a97b02626d3479ce20d32191
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2025-08-05 22:59:47 +0200

    refactor: modernize

--- src/main/java/com/questhelper/helpers/quests/impcatcher/ImpCatcher.java
+++ src/main/java/com/questhelper/helpers/quests/impcatcher/ImpCatcher.java
@@ -33,50 +33,53 @@ import com.questhelper.requirements.zone.ZoneRequirement;
 import com.questhelper.rewards.ExperienceReward;
 import com.questhelper.rewards.ItemReward;
 import com.questhelper.rewards.QuestPointReward;
-import com.questhelper.steps.*;
+import com.questhelper.steps.ConditionalStep;
+import com.questhelper.steps.DetailedQuestStep;
+import com.questhelper.steps.NpcStep;
+import com.questhelper.steps.ObjectStep;
+import com.questhelper.steps.QuestStep;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import net.runelite.api.Skill;
 import net.runelite.api.coords.WorldPoint;
 import net.runelite.api.gameval.ItemID;
 import net.runelite.api.gameval.NpcID;
 import net.runelite.api.gameval.ObjectID;
 
-import java.util.*;
-
 public class ImpCatcher extends BasicQuestHelper
 {
-	//Items Required
-	ItemRequirement blackBead, whiteBead, redBead, yellowBead;
+	// Required items
+	ItemRequirement blackBead;
+	ItemRequirement whiteBead;
+	ItemRequirement redBead;
+	ItemRequirement yellowBead;
 
-	QuestStep moveToTower, climbTower, turnInQuest, collectBeads;
+	// Zones
+	Zone towerSecond;
+	Zone towerThird;
 
-	Zone towerSecond, towerThird;
+	// Miscellaneus requirements
+	ZoneRequirement inTowerSecond;
+	ZoneRequirement inTowerThird;
 
-	ZoneRequirement inTowerSecond, inTowerThird;
+	// Steps
+	QuestStep moveToTower;
+	QuestStep climbTower;
+	QuestStep turnInQuest;
+	QuestStep collectBeads;
 
 	@Override
-	public Map<Integer, QuestStep> loadSteps()
+	protected void setupZones()
 	{
-		initializeRequirements();
-		setupSteps();
-
-		Map<Integer, QuestStep> steps = new HashMap<>();
-
-		ConditionalStep doQuest = new ConditionalStep(this, collectBeads);
-		doQuest.addStep(new Conditions(blackBead,whiteBead,redBead,yellowBead, inTowerThird), turnInQuest);
-		doQuest.addStep(new Conditions(blackBead,whiteBead,redBead,yellowBead, inTowerSecond), climbTower);
-		doQuest.addStep(new Conditions(blackBead,whiteBead,redBead,yellowBead), moveToTower);
-
-		steps.put(0, doQuest);
-
-		steps.put(1, doQuest);
-
-		return steps;
+		towerSecond = new Zone(new WorldPoint(3089, 3176, 1), new WorldPoint(3126, 3146, 1));
+		towerThird = new Zone(new WorldPoint(3089, 3176, 2), new WorldPoint(3126, 3146, 2));
 	}
 
-
-
 	@Override
-	protected void setupRequirements() {
+	protected void setupRequirements()
+	{
 		blackBead = new ItemRequirement("Black bead", ItemID.BLACK_BEAD);
 		whiteBead = new ItemRequirement("White bead", ItemID.WHITE_BEAD);
 		redBead = new ItemRequirement("Red bead", ItemID.RED_BEAD);
@@ -86,7 +89,8 @@ public class ImpCatcher extends BasicQuestHelper
 		inTowerThird = new ZoneRequirement(towerThird);
 	}
 
-	public void setupSteps(){
+	public void setupSteps()
+	{
 		collectBeads = new DetailedQuestStep(this, "Collect one of each bead. You can kill imps for these beads, or buy them on the Grand Exchange.",
 			blackBead, whiteBead, redBead, yellowBead);
 		moveToTower = new ObjectStep(this, ObjectID.FAI_WIZTOWER_SPIRALSTAIRS, new WorldPoint(3103, 3159, 0),
@@ -100,30 +104,34 @@ public class ImpCatcher extends BasicQuestHelper
 	}
 
 	@Override
-	protected void setupZones(){
-		towerSecond = new Zone(new WorldPoint(3089, 3176, 1), new WorldPoint(3126, 3146, 1));
-		towerThird = new Zone(new WorldPoint(3089, 3176, 2), new WorldPoint(3126, 3146, 2));
+	public Map<Integer, QuestStep> loadSteps()
+	{
+		initializeRequirements();
+		setupSteps();
+
+		var steps = new HashMap<Integer, QuestStep>();
+
+		var doQuest = new ConditionalStep(this, collectBeads);
+		doQuest.addStep(new Conditions(blackBead, whiteBead, redBead, yellowBead, inTowerThird), turnInQuest);
+		doQuest.addStep(new Conditions(blackBead, whiteBead, redBead, yellowBead, inTowerSecond), climbTower);
+		doQuest.addStep(new Conditions(blackBead, whiteBead, redBead, yellowBead), moveToTower);
+
+		steps.put(0, doQuest);
+
+		steps.put(1, doQuest);
+
+		return steps;
 	}
 
 	@Override
 	public List<ItemRequirement> getItemRequirements()
 	{
-		ArrayList<ItemRequirement> reqs = new ArrayList<>();
-		reqs.add(blackBead);
-		reqs.add(whiteBead);
-		reqs.add(redBead);
-		reqs.add(yellowBead);
-
-		return reqs;
-	}
-
-	@Override
-	public List<PanelDetails> getPanels()
-	{
-		List<PanelDetails> allSteps = new ArrayList<>();
-		allSteps.add(new PanelDetails("Bring Mizgog his beads", Arrays.asList(collectBeads, moveToTower, climbTower, turnInQuest),
-			blackBead, whiteBead, redBead, yellowBead));
-		return allSteps;
+		return List.of(
+			blackBead,
+			whiteBead,
+			redBead,
+			yellowBead
+		);
 	}
 
 	@Override
@@ -135,18 +143,44 @@ public class ImpCatcher extends BasicQuestHelper
 	@Override
 	public List<ExperienceReward> getExperienceRewards()
 	{
-		return Collections.singletonList(new ExperienceReward(Skill.MAGIC, 875));
+		return List.of(
+			new ExperienceReward(Skill.MAGIC, 875)
+		);
 	}
 
 	@Override
 	public List<ItemReward> getItemRewards()
 	{
-		return Collections.singletonList(new ItemReward("An Amulet of Accuracy", ItemID.AMULET_OF_ACCURACY, 1));
+		return List.of(
+			new ItemReward("An Amulet of Accuracy", ItemID.AMULET_OF_ACCURACY, 1)
+		);
 	}
 
 	@Override
 	public List<String> getCombatRequirements()
 	{
-		return Collections.singletonList("Imps (level 2) if you plan on collecting the beads yourself");
+		return List.of(
+			"Imps (level 2) if you plan on collecting the beads yourself"
+		);
+	}
+
+	@Override
+	public List<PanelDetails> getPanels()
+	{
+		var steps = new ArrayList<PanelDetails>();
+
+		steps.add(new PanelDetails("Bring Mizgog his beads", List.of(
+			collectBeads,
+			moveToTower,
+			climbTower,
+			turnInQuest
+		), List.of(
+			blackBead,
+			whiteBead,
+			redBead,
+			yellowBead
+		)));
+
+		return steps;
 	}
 }

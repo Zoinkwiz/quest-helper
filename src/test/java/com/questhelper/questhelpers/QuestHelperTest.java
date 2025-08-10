d7081425fc668e4feb6bf629a55cb1de766ebfb9
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2025-08-10 14:06:10 +0200

    test: opt in fight arena sidebar step

--- src/test/java/com/questhelper/questhelpers/QuestHelperTest.java
+++ src/test/java/com/questhelper/questhelpers/QuestHelperTest.java
@@ -277,6 +277,7 @@ public class QuestHelperTest extends MockedTest
 			QuestHelperQuest.STRONGHOLD_OF_SECURITY,
 			QuestHelperQuest.COOKS_ASSISTANT,
 			QuestHelperQuest.SHEEP_SHEARER,
+			QuestHelperQuest.FIGHT_ARENA,
 			QuestHelperQuest.IMP_CATCHER,
 			QuestHelperQuest.PRINCE_ALI_RESCUE
 		);

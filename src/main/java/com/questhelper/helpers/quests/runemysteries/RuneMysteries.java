b827259c324505f1c09da0afbabaca25e399390d
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2025-08-10 19:57:15 +0200

    fix: dialog step to climb down stairs

--- src/main/java/com/questhelper/helpers/quests/runemysteries/RuneMysteries.java
+++ src/main/java/com/questhelper/helpers/quests/runemysteries/RuneMysteries.java
@@ -113,6 +113,7 @@ public class RuneMysteries extends BasicQuestHelper
 
 		goF1ToF0LumbridgeCastle = new ObjectStep(this, ObjectID.SPIRALSTAIRSMIDDLE, new WorldPoint(3204, 3207, 1),
 			"Bring the Air Talisman to Sedridor in the Wizard Tower's basement.");
+		goF1ToF0LumbridgeCastle.addDialogStep("Climb down the stairs.");
 
 		goDownToSedridor = new ObjectStep(this, ObjectID.WIZARDS_TOWER_LADDERTOP, new WorldPoint(3104, 3162, 0), "Bring the Air Talisman to Sedridor in the Wizard Tower's basement.", airTalisman);
 		goDownToSedridor.addDialogStep("Have you any quests for me?");

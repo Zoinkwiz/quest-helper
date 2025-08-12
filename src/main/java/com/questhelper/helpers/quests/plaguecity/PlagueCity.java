76fa0d6cdec0c4f405f0b043eaed74d1a0d5b612
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2025-08-12 23:06:53 +0200

    nit: fix Martha/Ron last name typo

--- src/main/java/com/questhelper/helpers/quests/plaguecity/PlagueCity.java
+++ src/main/java/com/questhelper/helpers/quests/plaguecity/PlagueCity.java
@@ -241,7 +241,7 @@ public class PlagueCity extends BasicQuestHelper
 
 		enterMarthasHouse = new ObjectStep(this, ObjectID.REHNISONDOORSHUT, new WorldPoint(2531, 3328, 0), "Enter the tall house in north West Ardougne.");
 
-		talkToMartha = new NpcStep(this, NpcID.MARTHA_REHNISON, new WorldPoint(2531, 3331, 0), "Talk to Martha or Ted Renison");
+		talkToMartha = new NpcStep(this, NpcID.MARTHA_REHNISON, new WorldPoint(2531, 3331, 0), "Talk to Martha or Ted Rehnison");
 
 		goUpstairsInMarthasHouse = new ObjectStep(this, ObjectID.REHNISONSTAIRS, new WorldPoint(2528, 3333, 0), "Talk to Milli upstairs.");
 		talkToMilli = new NpcStep(this, NpcID.MILLI, new WorldPoint(2531, 3331, 1), "Talk to Milli.");

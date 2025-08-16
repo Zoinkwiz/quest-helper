e958f538a8ea53fbab51f4cf54c01522092143d2
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2025-08-16 13:37:49 +0200

    chore: remove x marks the spot exclusive

--- src/test/java/com/questhelper/questhelpers/QuestHelperTest.java
+++ src/test/java/com/questhelper/questhelpers/QuestHelperTest.java
@@ -297,7 +297,6 @@ public class QuestHelperTest extends MockedTest
 
 		// If you add a quest to this list, then this unit test will *only* test this quest
 		Set<QuestHelperQuest> exclusiveQuests = Set.of(
-			QuestHelperQuest.X_MARKS_THE_SPOT
 		);
 
 		when(questHelperConfig.solvePuzzles()).thenReturn(true);

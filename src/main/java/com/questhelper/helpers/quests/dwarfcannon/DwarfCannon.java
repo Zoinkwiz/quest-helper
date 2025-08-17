9d8139094b9fc57cc780852e11796bd6ae241800
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2025-08-17 15:52:36 +0200

    chore: add myself to license

--- src/main/java/com/questhelper/helpers/quests/dwarfcannon/DwarfCannon.java
+++ src/main/java/com/questhelper/helpers/quests/dwarfcannon/DwarfCannon.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2020, Zoinkwiz
+ * Copyright (c) 2025, pajlada <https://github.com/pajlada>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without

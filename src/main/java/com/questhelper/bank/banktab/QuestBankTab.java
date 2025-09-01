3c2cb01214f47fe60c80a29a2f7c9accfc4ff180
pajlada <rasmus.karlsson@pajlada.com>
2025-09-01 16:24:21 +0200

    fix: bank withdrawing 232 (#2299)
    * fix: Quest Helper tab bank withdrawing
    
    See https://github.com/runelite/runelite/commit/96ddec45d3812730278ccb867241ddc212afb483
    
    * maybe fix potion store? untested
    
    * fix: Allow vials to work for PotionStorage
    
    * fix: Use getIdx for potionstorage
    
    ---------
    
    Co-authored-by: Zoinkwiz <zoinkwiz@hotmail.co.uk>

--- src/main/java/com/questhelper/bank/banktab/QuestBankTab.java
+++ src/main/java/com/questhelper/bank/banktab/QuestBankTab.java
@@ -279,18 +279,12 @@ public class QuestBankTab
 					return;
 				}
 
-				idx = potionStorage.find(w.getItemId());
-				if (idx == VIAL_IDX)
+				idx = potionStorage.getIdx(w.getItemId());
+				if (idx > -1)
 				{
 					potionStorage.prepareWidgets();
 					menu.setParam1(InterfaceID.Bankmain.POTIONSTORE_ITEMS);
-					menu.setParam0(VIAL_IDX);
-				}
-				else if (idx > -1)
-				{
-					potionStorage.prepareWidgets();
-					menu.setParam1(InterfaceID.Bankmain.POTIONSTORE_ITEMS);
-					menu.setParam0(idx * COMPONENTS_PER_POTION);
+					menu.setParam0(idx);
 				}
 			}
 		}
@@ -580,41 +574,31 @@ public class QuestBankTab
 						break;
 				}
 				// ~script669
-				int opIdx = 0;
-				c.setAction(opIdx++, "Withdraw-" + suffix);
+				c.setAction(0, "Withdraw-" + suffix);
 				if (quantityType != 0)
 				{
-					c.setAction(opIdx++, "Withdraw-1");
+					c.setAction(1, "Withdraw-1");
 				}
-				if (quantityType != 1)
+				c.setAction(2, "Withdraw-5");
+				c.setAction(3, "Withdraw-10");
+				if (requestQty > 0)
 				{
-					c.setAction(opIdx++, "Withdraw-5");
+					c.setAction(4, "Withdraw-" + requestQty);
 				}
-				if (quantityType != 2)
-				{
-					c.setAction(opIdx++, "Withdraw-10");
-				}
-				if (quantityType != 3 && requestQty > 0)
-				{
-					c.setAction(opIdx++, "Withdraw-" + requestQty);
-				}
-				c.setAction(opIdx++, "Withdraw-X");
-				if (quantityType != 4)
-				{
-					c.setAction(opIdx++, "Withdraw-All");
-				}
-				c.setAction(opIdx++, "Withdraw-All-but-1");
+				c.setAction(5, "Withdraw-X");
+				c.setAction(6, "Withdraw-All");
+				c.setAction(7, "Withdraw-All-but-1");
 				if (!isPotStorage && client.getVarbitValue(VarbitID.BANK_BANKOPS_TOGGLE_ON) == 1 && def.getIntValue(ParamID.BANK_AUTOCHARGE) != -1)
 				{
-					c.setAction(opIdx++, "Configure-Charges");
+					c.setAction(8, "Configure-Charges");
 				}
 				if (!isPotStorage && client.getVarbitValue(VarbitID.BANK_LEAVEPLACEHOLDERS) == 0)
 				{
-					c.setAction(opIdx++, "Placeholder");
+					c.setAction(9, "Placeholder");
 				}
 				if (!isPotStorage)
 				{
-					c.setAction(9, "Examine");
+					c.setAction(10, "Examine");
 				}
 				c.setOpacity(0);
 			}

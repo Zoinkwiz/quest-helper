3cfebfaa619ced7499b63be11f90ce605f36ec44
Zoinkwiz <zoinkwiz@hotmail.co.uk>
2025-11-21 21:55:32 +0000

    added wiki guide link for sea charting

--- src/main/java/com/questhelper/questinfo/ExternalQuestResources.java
+++ src/main/java/com/questhelper/questinfo/ExternalQuestResources.java
@@ -300,7 +300,9 @@ public enum ExternalQuestResources
 	// Skills
 	AGILITY("https://oldschool.runescape.wiki/w/Agility_training"),
 	WOODCUTTING_MEMBER("https://oldschool.runescape.wiki/w/Pay-to-play_Woodcutting_training"),
-	WOODCUTTING("https://oldschool.runescape.wiki/w/Free-to-play_Woodcutting_training");
+	WOODCUTTING("https://oldschool.runescape.wiki/w/Free-to-play_Woodcutting_training"),
+	SEA_CHARTING("https://oldschool.runescape.wiki/w/Sea_charting")
+	;
 
 	ExternalQuestResources()
 	{

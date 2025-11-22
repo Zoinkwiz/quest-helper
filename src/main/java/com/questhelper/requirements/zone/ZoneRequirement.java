1934304d893bdb5ddb1b01c9ef991148f060ddb3
Toby T. van Willegen <git@tobyvanwillegen.nl>
2025-11-22 19:51:27 +0100

    Fix: Make ZoneRequirement work with Sailing.

--- src/main/java/com/questhelper/requirements/zone/ZoneRequirement.java
+++ src/main/java/com/questhelper/requirements/zone/ZoneRequirement.java
@@ -68,7 +68,7 @@ public class ZoneRequirement extends AbstractRequirement
 	 */
 	public ZoneRequirement(String displayText, boolean checkNotInZone, Zone zone)
 	{
-		assert(zone != null);
+		assert (zone != null);
 		this.displayText = displayText;
 		this.checkInZone = !checkNotInZone; // This was originally 'checkNotInZone' so we have to maintain that behavior
 		this.zones = QuestUtil.toArrayList(zone);
@@ -76,28 +76,28 @@ public class ZoneRequirement extends AbstractRequirement
 
 	public ZoneRequirement(WorldPoint... worldPoints)
 	{
-		assert(Utils.varargsNotNull(worldPoints));
+		assert (Utils.varargsNotNull(worldPoints));
 		this.zones = Stream.of(worldPoints).map(Zone::new).collect(QuestUtil.collectToArrayList());
 		this.checkInZone = true;
 	}
 
 	public ZoneRequirement(Zone... zone)
 	{
-		assert(Utils.varargsNotNull(zone));
+		assert (Utils.varargsNotNull(zone));
 		this.zones = QuestUtil.toArrayList(zone);
 		this.checkInZone = true;
 	}
 
 	public ZoneRequirement(boolean checkInZone, Zone... zone)
 	{
-		assert(Utils.varargsNotNull(zone));
+		assert (Utils.varargsNotNull(zone));
 		this.zones = QuestUtil.toArrayList(zone);
 		this.checkInZone = checkInZone;
 	}
 
 	public ZoneRequirement(boolean checkInZone, WorldPoint... worldPoints)
 	{
-		assert(Utils.varargsNotNull(worldPoints));
+		assert (Utils.varargsNotNull(worldPoints));
 		this.zones = Stream.of(worldPoints).map(Zone::new).collect(QuestUtil.collectToArrayList());
 		this.checkInZone = checkInZone;
 	}

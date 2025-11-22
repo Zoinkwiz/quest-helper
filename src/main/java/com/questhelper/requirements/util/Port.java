865397d9d290bc667c6cbcbbb3e93a230f7e45a4
Toby T. van Willegen <git@tobyvanwillegen.nl>
2025-11-22 19:52:29 +0100

    Feature: Add requirement to check if a boat is at a Port.

---
+++ src/main/java/com/questhelper/requirements/util/Port.java
@@ -0,0 +1,65 @@
+/*
+ *
+ *  * Copyright (c) 2025, TTvanWillegen
+ *  * All rights reserved.
+ *  *
+ *  * Redistribution and use in source and binary forms, with or without
+ *  * modification, are permitted provided that the following conditions are met:
+ *  *
+ *  * 1. Redistributions of source code must retain the above copyright notice, this
+ *  *    list of conditions and the following disclaimer.
+ *  * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *  *    this list of conditions and the following disclaimer in the documentation
+ *  *    and/or other materials provided with the distribution.
+ *  *
+ *  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ *  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ *  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ *  * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ *  * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ *  * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ *  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+package com.questhelper.requirements.util;
+
+import com.questhelper.requirements.zone.Zone;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import net.runelite.api.coords.WorldPoint;
+
+@AllArgsConstructor
+@Getter
+public enum Port
+{
+	PORT_SARIM(0, "Port Sarim", new WorldPoint(3048, 3186,0), new Zone(new WorldPoint(3045, 3183, 0), new WorldPoint(3061, 3208, 0))),
+	PANDEMONIUM(1, "the Pandemonium", new WorldPoint(3069, 2981,0), new Zone(new WorldPoint(3065, 2974, 0), new WorldPoint(3084, 2998, 0))),
+	MUSA_POINT(3, "Musa Point", new WorldPoint(2961, 3152,0), new Zone(new WorldPoint(2974, 3156, 0), new WorldPoint(2956, 3144, 0)));
+	private final int id;
+	private final String name;
+	private final WorldPoint buoy;
+	private final Zone dockZone;
+
+	public int getId()
+	{
+		return id;
+	}
+
+	public String getName()
+	{
+		return name;
+	}
+
+	public Zone getDockZone()
+	{
+		return dockZone;
+	}
+
+	public WorldPoint getBuoy()
+	{
+		return buoy;
+	}
+}

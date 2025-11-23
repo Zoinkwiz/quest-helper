71f06a5e462e21333f6fb51b19ab02b4ef222329
Toby T. van Willegen <git@tobyvanwillegen.nl>
2025-11-23 20:51:07 +0100

    Fix: move choice value and dialog string around.

--- src/main/java/com/questhelper/steps/QuestStep.java
+++ src/main/java/com/questhelper/steps/QuestStep.java
@@ -370,8 +370,8 @@ public abstract class QuestStep implements Module
 
 	public QuestStep addDialogConsideringLastLineCondition(String dialogString, String choiceValue)
 	{
-		DialogChoiceStep choice = new DialogChoiceStep(questHelper.getConfig(), dialogString);
-		choice.setExpectedPreviousLine(choiceValue);
+		DialogChoiceStep choice = new DialogChoiceStep(questHelper.getConfig(), choiceValue);
+		choice.setExpectedPreviousLine(dialogString);
 		choices.addChoice(choice);
 		return this;
 	}

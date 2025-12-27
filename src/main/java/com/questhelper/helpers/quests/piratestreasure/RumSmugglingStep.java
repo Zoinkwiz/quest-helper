ace1fe82498b0b0d3657cc4ff9e93b1ac7e4e87d
Repcal <repcalxd@gmail.com>
2025-12-26 23:32:55 -0700

    fix: Update dialogs

--- src/main/java/com/questhelper/helpers/quests/piratestreasure/RumSmugglingStep.java
+++ src/main/java/com/questhelper/helpers/quests/piratestreasure/RumSmugglingStep.java
@@ -201,8 +201,8 @@ public class RumSmugglingStep extends ConditionalStep
 			"Head back to Port Sarim. Pay the Customs Officer to sail there.", new ItemRequirement("Coins", ItemCollections.COINS, 30));
 		talkToCustomsOfficer.addDialogStep("Thank you, I'll be on my way");
 		talkToCustomsOfficer.addDialogStep("Can I journey on this ship?");
-		talkToCustomsOfficer.addDialogStep("Search away, I have nothing to hide.");
-		talkToCustomsOfficer.addDialogStep("Ok.");
+		talkToCustomsOfficer.addDialogStep("Search away. I have nothing to hide.");
+		talkToCustomsOfficer.addDialogStep("Okay.");
 
 		getWhiteApron = new DetailedQuestStep(getQuestHelper(), new WorldPoint(3016, 3229, 0),
 			"Grab the white apron from the Fishing Shop.", whiteApronHanging);

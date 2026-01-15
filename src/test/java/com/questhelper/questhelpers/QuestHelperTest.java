bf14ea9ce7b1373f75e0fdf66b20180a4dc3a514
pajlada <rasmus.karlsson@pajlada.com>
2026-01-15 12:20:41 +0100

    polish: Nature Spirit (#2514)
    * refactor: modernize
    
    * test: opt into sidebar tests
    
    * fix: add missing sidebar/substeps
    
    * improve messaging on returning to drezel first time
    
    * nit: highlight mushroomed log
    
    this _could_ be expanded to be even more helpful to highlight normal logs until a muishroom log appears, but this is ok.

--- src/test/java/com/questhelper/questhelpers/QuestHelperTest.java
+++ src/test/java/com/questhelper/questhelpers/QuestHelperTest.java
@@ -336,6 +336,7 @@ public class QuestHelperTest extends MockedTest
 			QuestHelperQuest.CURRENT_AFFAIRS,
 			QuestHelperQuest.HOLY_GRAIL,
 			QuestHelperQuest.PRIEST_IN_PERIL,
+			QuestHelperQuest.NATURE_SPIRIT,
 			QuestHelperQuest.PRINCE_ALI_RESCUE,
 			QuestHelperQuest.HERB_RUN
 		);

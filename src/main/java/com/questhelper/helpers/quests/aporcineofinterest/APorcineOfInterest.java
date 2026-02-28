0593e20fc04f210af6a80bc8ce79a1f6d93d04d0
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2026-02-28 13:50:37 +0100

    refactor: modernize

--- src/main/java/com/questhelper/helpers/quests/aporcineofinterest/APorcineOfInterest.java
+++ src/main/java/com/questhelper/helpers/quests/aporcineofinterest/APorcineOfInterest.java
@@ -36,62 +36,60 @@ import com.questhelper.rewards.ExperienceReward;
 import com.questhelper.rewards.ItemReward;
 import com.questhelper.rewards.QuestPointReward;
 import com.questhelper.rewards.UnlockReward;
-import com.questhelper.steps.*;
+import com.questhelper.steps.ConditionalStep;
+import com.questhelper.steps.NpcStep;
+import com.questhelper.steps.ObjectStep;
+import com.questhelper.steps.QuestStep;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import net.runelite.api.Skill;
 import net.runelite.api.coords.WorldPoint;
 import net.runelite.api.gameval.ItemID;
 import net.runelite.api.gameval.NpcID;
 import net.runelite.api.gameval.ObjectID;
 
-import java.util.*;
-
 public class APorcineOfInterest extends BasicQuestHelper
 {
-	//Items Required
-	ItemRequirement rope, slashItem, reinforcedGoggles, combatGear, hoof;
+	// Required items
+	ItemRequirement rope;
+	ItemRequirement slashItem;
+	ItemRequirement combatGear;
 
-	//Items Recommended
-	ItemRequirement draynorTeleport, faladorFarmTeleport;
+	// Recommended items
+	ItemRequirement draynorTeleport;
+	ItemRequirement faladorFarmTeleport;
 
-	Requirement inCave;
+	// Mid-quest item requirements
+	ItemRequirement reinforcedGoggles;
+	ItemRequirement hoof;
 
-	DetailedQuestStep readNotice, talkToSarah, useRopeOnHole, enterHole, investigateSkeleton, talkToSpria, enterHoleAgain, killSourhog,
-		enterHoleForFoot, cutOffFoot, returnToSarah, returnToSpria;
-
-	//Zones
+	// Zones
 	Zone cave;
 
+	// Miscellaneous requirements
+	Requirement inCave;
+
+	// Steps
+	ObjectStep readNotice;
+	NpcStep talkToSarah;
+	ObjectStep useRopeOnHole;
+	ObjectStep enterHole;
+	ObjectStep investigateSkeleton;
+	NpcStep talkToSpria;
+	ObjectStep enterHoleAgain;
+	NpcStep killSourhog;
+	ObjectStep enterHoleForFoot;
+	ObjectStep cutOffFoot;
+	NpcStep returnToSarah;
+	NpcStep returnToSpria;
+
+
 	@Override
-	public Map<Integer, QuestStep> loadSteps()
+	protected void setupZones()
 	{
-		initializeRequirements();
-		setupConditions();
-		setupSteps();
-		Map<Integer, QuestStep> steps = new HashMap<>();
-
-		steps.put(0, readNotice);
-		steps.put(5, talkToSarah);
-		steps.put(10, useRopeOnHole);
-
-		ConditionalStep investigateCave = new ConditionalStep(this, enterHole);
-		investigateCave.addStep(inCave, investigateSkeleton);
-
-		steps.put(15, investigateCave);
-
-		steps.put(20, talkToSpria);
-
-		ConditionalStep goKillSourhog = new ConditionalStep(this, enterHoleAgain);
-		goKillSourhog.addStep(inCave, killSourhog);
-
-		steps.put(25, goKillSourhog);
-
-		ConditionalStep getFootSteps = new ConditionalStep(this, enterHoleForFoot);
-		getFootSteps.addStep(hoof, returnToSarah);
-		getFootSteps.addStep(inCave, cutOffFoot);
-
-		steps.put(30, getFootSteps);
-		steps.put(35, returnToSpria);
-		return steps;
+		cave = new Zone(new WorldPoint(3152, 9669, 0), new WorldPoint(3181, 9720, 0));
 	}
 
 	@Override
@@ -116,16 +114,7 @@ public class APorcineOfInterest extends BasicQuestHelper
 		draynorTeleport = new ItemRequirement("Teleport to north Draynor", ItemID.TELETAB_DRAYNOR);
 		draynorTeleport.addAlternates(ItemCollections.AMULET_OF_GLORIES);
 		faladorFarmTeleport = new ItemRequirement("Teleport to Falador Farm", ItemCollections.EXPLORERS_RINGS);
-	}
 
-	@Override
-	protected void setupZones()
-	{
-		cave = new Zone(new WorldPoint(3152, 9669, 0), new WorldPoint(3181, 9720, 0));
-	}
-
-	public void setupConditions()
-	{
 		inCave = new ZoneRequirement(cave);
 	}
 
@@ -153,7 +142,7 @@ public class APorcineOfInterest extends BasicQuestHelper
 
 		enterHoleForFoot = new ObjectStep(this, ObjectID.PORCINE_HOLE, new WorldPoint(3151, 3348, 0), "Climb down into the Strange Hole east of Draynor Manor.", slashItem);
 		cutOffFoot = new ObjectStep(this, ObjectID.PORCINE_DEAD_SOURHOG, "Cut off Sourhog's foot.", slashItem);
-		((ObjectStep) cutOffFoot).addAlternateObjects(ObjectID.PORCINE_DEAD_SOURHOG_9);
+		cutOffFoot.addAlternateObjects(ObjectID.PORCINE_DEAD_SOURHOG_9);
 		cutOffFoot.addSubSteps(enterHoleForFoot);
 
 		returnToSarah = new NpcStep(this, NpcID.FARMING_SHOPKEEPER_1, new WorldPoint(3033, 3293, 0), "Return to Sarah in the South Falador Farm.", hoof);
@@ -163,21 +152,64 @@ public class APorcineOfInterest extends BasicQuestHelper
 	}
 
 	@Override
+	public Map<Integer, QuestStep> loadSteps()
+	{
+		initializeRequirements();
+		setupSteps();
+
+		var steps = new HashMap<Integer, QuestStep>();
+
+		steps.put(0, readNotice);
+		steps.put(5, talkToSarah);
+		steps.put(10, useRopeOnHole);
+
+		var investigateCave = new ConditionalStep(this, enterHole);
+		investigateCave.addStep(inCave, investigateSkeleton);
+
+		steps.put(15, investigateCave);
+
+		steps.put(20, talkToSpria);
+
+		var goKillSourhog = new ConditionalStep(this, enterHoleAgain);
+		goKillSourhog.addStep(inCave, killSourhog);
+
+		steps.put(25, goKillSourhog);
+
+		var getFootSteps = new ConditionalStep(this, enterHoleForFoot);
+		getFootSteps.addStep(hoof, returnToSarah);
+		getFootSteps.addStep(inCave, cutOffFoot);
+
+		steps.put(30, getFootSteps);
+		steps.put(35, returnToSpria);
+
+		return steps;
+	}
+
+	@Override
 	public List<ItemRequirement> getItemRequirements()
 	{
-		return Arrays.asList(rope, slashItem, combatGear);
+		return List.of(
+			rope,
+			slashItem,
+			combatGear
+		);
 	}
 
 	@Override
 	public List<ItemRequirement> getItemRecommended()
 	{
-		return Arrays.asList(draynorTeleport, faladorFarmTeleport);
+		return List.of(
+			draynorTeleport,
+			faladorFarmTeleport
+		);
 	}
 
 	@Override
 	public List<String> getCombatRequirements()
 	{
-		return Collections.singletonList("Sourhog (level 37)");
+		return List.of(
+			"Sourhog (level 37)"
+		);
 	}
 
 	@Override
@@ -189,28 +221,53 @@ public class APorcineOfInterest extends BasicQuestHelper
 	@Override
 	public List<ExperienceReward> getExperienceRewards()
 	{
-		return Collections.singletonList(new ExperienceReward(Skill.SLAYER, 1000));
+		return List.of(
+			new ExperienceReward(Skill.SLAYER, 1000)
+		);
 	}
 
 	@Override
 	public List<ItemReward> getItemRewards()
 	{
-		return Collections.singletonList(new ItemReward("Coins", ItemID.COINS, 5000));
+		return List.of(
+			new ItemReward("Coins", ItemID.COINS, 5000)
+		);
 	}
 
 	@Override
 	public List<UnlockReward> getUnlockRewards()
 	{
-		return Arrays.asList(new UnlockReward("30 Slayer Points"), new UnlockReward("Access to Sourhog Cave"), new UnlockReward("Sourhogs can be assigned as a slayer task by Spria"));
+		return List.of(
+			new UnlockReward("30 Slayer Points"),
+			new UnlockReward("Access to Sourhog Cave"),
+			new UnlockReward("Sourhogs can be assigned as a slayer task by Spria")
+		);
 	}
 
 	@Override
 	public List<PanelDetails> getPanels()
 	{
-		List<PanelDetails> allSteps = new ArrayList<>();
-		allSteps.add(new PanelDetails("Starting off", Arrays.asList(readNotice, talkToSarah, useRopeOnHole,
-			enterHole, investigateSkeleton, talkToSpria, enterHoleAgain, killSourhog, cutOffFoot, returnToSarah,
-			returnToSpria), rope, slashItem, combatGear));
-		return allSteps;
+		var sections = new ArrayList<PanelDetails>();
+
+		// TODO: Rename section since it's just a single section
+		sections.add(new PanelDetails("Starting off", List.of(
+			readNotice,
+			talkToSarah,
+			useRopeOnHole,
+			enterHole,
+			investigateSkeleton,
+			talkToSpria,
+			enterHoleAgain,
+			killSourhog,
+			cutOffFoot,
+			returnToSarah,
+			returnToSpria
+		), List.of(
+			rope,
+			slashItem,
+			combatGear
+		)));
+
+		return sections;
 	}
 }

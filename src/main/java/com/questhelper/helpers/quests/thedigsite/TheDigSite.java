b9a9201d61f1a54519c414f26cef46e3e0af36b3
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2026-02-28 16:46:00 +0100

    chore: add myself to license blurb

--- src/main/java/com/questhelper/helpers/quests/thedigsite/TheDigSite.java
+++ src/main/java/com/questhelper/helpers/quests/thedigsite/TheDigSite.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2020, Zoinkwiz
+ * Copyright (c) 2026, pajlada <rasmus.karlsson@pajlada.com>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without

94acc617e99fe8029ea62f9b8bceaf1ece0a75ea
Zoinkwiz <zoinkwiz@hotmail.co.uk>
2026-08-11 09:49:54 +0100

    Update to 4.17.0

--- src/main/java/com/questhelper/managers/NewVersionManager.java
+++ src/main/java/com/questhelper/managers/NewVersionManager.java
@@ -44,7 +44,7 @@ public class NewVersionManager
 
 	private final String LAST_VERSION_SEEN_CONFIG_KEY = "lastversionchecked";
 
-	private final String UPDATE_CHAT_TEXT = "Quest Helper has been updated to 4.15.1! You can now filter by leagues regions, and the League's Helper is improved.";
+	private final String UPDATE_CHAT_TEXT = "Quest Helper has been updated to 4.17.0! <col=FF2400>The Blood Moon Rises</col> (ty pajlada!) and <col=123456>Fallen From Grace</col> are now both added!";
 
 	public void updateChatWithNotificationIfNewVersion()
 	{

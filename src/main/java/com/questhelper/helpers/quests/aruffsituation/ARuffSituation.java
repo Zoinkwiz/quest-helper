c47643529dc37d300bb236f579fc638d40e2e9f9
Nick Heise <nick@heise.org>
2026-09-18 00:15:48 -0700

    update header attribution

--- src/main/java/com/questhelper/helpers/quests/aruffsituation/ARuffSituation.java
+++ src/main/java/com/questhelper/helpers/quests/aruffsituation/ARuffSituation.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2026, nwheise <https://github.com/nwheise>
+ * Copyright (c) 2026, Zoinkwiz <https://github.com/Zoinkwiz>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
